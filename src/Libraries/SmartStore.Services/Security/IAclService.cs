d3daee8ebbe7d992d2fbd4d404d6978ae4a462f6
Marcus Gesing <home@footwear-box.de>
2013-06-01 20:21:34 +0200

    Renamed some comments

--- src/Libraries/SmartStore.Services/Security/IAclService.cs
+++ src/Libraries/SmartStore.Services/Security/IAclService.cs
@@ -47,6 +47,7 @@ namespace SmartStore.Services.Security
         /// </summary>
         /// <typeparam name="T">Type</typeparam>
         /// <param name="entity">Entity</param>
+		/// <param name="customerRoleId">Customer role id</param>
         void InsertAclRecord<T>(T entity, int customerRoleId) where T : BaseEntity, IAclSupported;
 
         /// <summary>

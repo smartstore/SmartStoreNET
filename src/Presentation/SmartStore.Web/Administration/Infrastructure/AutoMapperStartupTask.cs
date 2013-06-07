cbcd2de53e9150df0311c501de3d4045747b115e
Marcus Gesing <home@footwear-box.de>
2013-06-07 11:39:18 +0200

    Updated AutoMapperStartupTask according to the previous changeset

--- src/Presentation/SmartStore.Web/Administration/Infrastructure/AutoMapperStartupTask.cs
+++ src/Presentation/SmartStore.Web/Administration/Infrastructure/AutoMapperStartupTask.cs
@@ -130,6 +130,8 @@ namespace SmartStore.Admin.Infrastructure
                 .ForMember(dest => dest.AllowedTokens, mo => mo.Ignore())
                 .ForMember(dest => dest.Locales, mo => mo.Ignore())
                 .ForMember(dest => dest.AvailableEmailAccounts, mo => mo.Ignore())
+				.ForMember(dest => dest.AvailableStores, mo => mo.Ignore())
+				.ForMember(dest => dest.StoreName, mo => mo.Ignore())
                 .ForMember(dest => dest.CustomProperties, mo => mo.Ignore());
             Mapper.CreateMap<MessageTemplateModel, MessageTemplate>();
             //queued email

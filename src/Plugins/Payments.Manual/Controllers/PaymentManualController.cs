c95a13bda002198c7cca7554f37e9f8d8fccb0ae
Marcus Gesing <home@footwear-box.de>
2013-06-26 15:00:46 +0200

    Payment plugin "Manual" configuration per store

--- src/Plugins/Payments.Manual/Controllers/PaymentManualController.cs
+++ src/Plugins/Payments.Manual/Controllers/PaymentManualController.cs
@@ -2,56 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using SmartStore.Core;
 using SmartStore.Plugin.Payments.Manual.Models;
 using SmartStore.Plugin.Payments.Manual.Validators;
 using SmartStore.Services.Configuration;
 using SmartStore.Services.Localization;
 using SmartStore.Services.Payments;
+using SmartStore.Services.Stores;
 using SmartStore.Web.Framework;
 using SmartStore.Web.Framework.Controllers;
+using SmartStore.Web.Framework.Settings;
 
 namespace SmartStore.Plugin.Payments.Manual.Controllers
 {
     public class PaymentManualController : PaymentControllerBase
     {
+		private readonly IWorkContext _workContext;
+		private readonly IStoreService _storeService;
         private readonly ISettingService _settingService;
         private readonly ILocalizationService _localizationService;
-        private readonly ManualPaymentSettings _manualPaymentSettings;
 
-        public PaymentManualController(ISettingService settingService, 
-            ILocalizationService localizationService, ManualPaymentSettings manualPaymentSettings)
+		public PaymentManualController(IWorkContext workContext,
+			IStoreService storeService, 
+			ISettingService settingService, 
+            ILocalizationService localizationService)
         {
+			this._workContext = workContext;
+			this._storeService = storeService;
             this._settingService = settingService;
             this._localizationService = localizationService;
-            this._manualPaymentSettings = manualPaymentSettings;
         }
         
         [AdminAuthorize]
         [ChildActionOnly]
         public ActionResult Configure()
         {
+			//load settings for a chosen store scope
+			var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+			var manualPaymentSettings = _settingService.LoadSetting<ManualPaymentSettings>(storeScope);
+
             var model = new ConfigurationModel();
-            model.TransactModeId = Convert.ToInt32(_manualPaymentSettings.TransactMode);
-            model.AdditionalFee = _manualPaymentSettings.AdditionalFee;
-            model.TransactModeValues = _manualPaymentSettings.TransactMode.ToSelectList();
-            
+            model.TransactMode = Convert.ToInt32(manualPaymentSettings.TransactMode);
+            model.AdditionalFee = manualPaymentSettings.AdditionalFee;
+			model.AdditionalFeePercentage = manualPaymentSettings.AdditionalFeePercentage;
+            model.TransactModeValues = manualPaymentSettings.TransactMode.ToSelectList();
+
+			var storeDependingSettings = new StoreDependingSettingHelper(ViewData);
+			storeDependingSettings.GetOverrideKeys(manualPaymentSettings, model, storeScope, _settingService);
+
             return View("SmartStore.Plugin.Payments.Manual.Views.PaymentManual.Configure", model);
         }
 
         [HttpPost]
         [AdminAuthorize]
         [ChildActionOnly]
-        public ActionResult Configure(ConfigurationModel model)
+        public ActionResult Configure(ConfigurationModel model, FormCollection form)
         {
             if (!ModelState.IsValid)
                 return Configure();
+
+			//load settings for a chosen store scope
+			var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+			var manualPaymentSettings = _settingService.LoadSetting<ManualPaymentSettings>(storeScope);
             
             //save settings
-            _manualPaymentSettings.TransactMode = (TransactMode)model.TransactModeId;
-            _manualPaymentSettings.AdditionalFee = model.AdditionalFee;
-            _settingService.SaveSetting(_manualPaymentSettings);
-            
-            model.TransactModeValues = _manualPaymentSettings.TransactMode.ToSelectList();
+            manualPaymentSettings.TransactMode = (TransactMode)model.TransactMode;
+            manualPaymentSettings.AdditionalFee = model.AdditionalFee;
+			manualPaymentSettings.AdditionalFeePercentage = model.AdditionalFeePercentage;
+            model.TransactModeValues = manualPaymentSettings.TransactMode.ToSelectList();
+
+			var storeDependingSettings = new StoreDependingSettingHelper(ViewData);
+			storeDependingSettings.UpdateSettings(manualPaymentSettings, form, storeScope, _settingService);
+
+			//now clear settings cache
+			_settingService.ClearCache();
 
             return View("SmartStore.Plugin.Payments.Manual.Views.PaymentManual.Configure", model);
         }

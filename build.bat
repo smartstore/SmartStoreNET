42f569f7f2737f491597a1e2b050fdcc97e36eac
Murat Cakir <muratc@smartstore.de>
2013-09-11 00:20:56 +0200

    Added SlnName property to build script

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ set MSBuildPath=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
 @IF NOT EXIST %MSBuildPath% @ECHO COULDN'T FIND MSBUILD: %MSBuildPath% (Is .NET 4 installed?)
 
-%MSBuildPath% SmartStoreNET.proj /p:DebugSymbols=false /p:DebugType=None /maxcpucount %*
+%MSBuildPath% SmartStoreNET.proj /p:DebugSymbols=false /p:DebugType=None /P:SlnName=SmartStoreNET /maxcpucount %*

fcbb4e40f3e0b218749c7a3e067aac42831ab2c5
Murat Cakir <muratc@smartstore.de>
2013-10-31 22:10:04 +0100

    Updated ASP.NET MVC 4 to MVC 5 (including dependent updates to AutoMapper, DotNetOpenAuth, FluentValidation etc.)

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -12,4 +12,4 @@
 [assembly: AssemblyVersion("1.2.1.0")]
 
 [assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.0.0")]

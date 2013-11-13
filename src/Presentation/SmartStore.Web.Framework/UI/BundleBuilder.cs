9989ca93c4c20e14305824a3c4c160cbf224394a
Murat Cakir <muratc@smartstore.de>
2013-11-13 01:29:40 +0100

    Removed some unused resources

--- src/Presentation/SmartStore.Web.Framework/UI/BundleBuilder.cs
+++ src/Presentation/SmartStore.Web.Framework/UI/BundleBuilder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Optimization;
@@ -58,6 +60,7 @@ namespace SmartStore.Web.Framework.UI
                         else
                         {
                             files.Each(x => bundle.Include(x, new CssRewriteUrlTransform()));
+                            //bundle.Transforms.Add(new CssUrlTransform());
                         }
 
                         BundleTable.Bundles.Add(bundle);
@@ -114,6 +117,51 @@ namespace SmartStore.Web.Framework.UI
             {
                 return files;
             }
+        }
+
+        public class CssUrlTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                Regex pattern = new Regex(@"url\s*\(\s*([""']?)([^:)]+)\1\s*\)", RegexOptions.IgnoreCase);
+
+                response.Content = string.Empty;
+
+                // open each of the files
+                foreach (var file in response.Files)
+                {
+                    using (var reader = new StreamReader(file.VirtualFile.Open()))
+                    {
+                        var contents = reader.ReadToEnd();
+
+                        // apply the RegEx to the file (to change relative paths)
+                        var matches = pattern.Matches(contents);
+
+                        if (matches.Count > 0)
+                        {
+                            var directoryPath = VirtualPathUtility.GetDirectory(file.IncludedVirtualPath);
+
+                            foreach (Match match in matches)
+                            {
+                                // this is a path that is relative to the CSS file
+                                var imageRelativePath = match.Groups[2].Value;
+
+                                // get the image virtual path
+                                var imageVirtualPath = VirtualPathUtility.Combine(directoryPath, imageRelativePath);
+
+                                // convert the image virtual path to absolute
+                                var quote = match.Groups[1].Value;
+                                var replace = String.Format("url({0}{1}{0})", quote, VirtualPathUtility.ToAbsolute(imageVirtualPath));
+                                contents = contents.Replace(match.Groups[0].Value, replace);
+                            }
+
+                        }
+                        // copy the result into the response.
+                        response.Content = String.Format("{0}\r\n{1}", response.Content, contents);
+                    }
+                }
+            }
+
         }
     }
 

7022ab3a9b088351fa1ccf4b6c29f97add4849d4
Marcus Gesing <home@footwear-box.de>
2013-11-23 18:48:00 +0100

    Adding some core components to framework project

---
+++ src/Presentation/SmartStore.Web.Framework/WebApi/WebApiExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartStore.Web.Framework.WebApi
+{
+	public static class WebApiExtension
+	{
+		public static HttpResponseException ExceptionInvalidModelState(this ApiController apiController)
+		{
+			return new HttpResponseException(apiController.Request.CreateErrorResponse(HttpStatusCode.BadRequest, apiController.ModelState));
+		}
+		public static HttpResponseException ExceptionUnprocessableEntity(this ApiController apiController, string message)
+		{
+			return new HttpResponseException(apiController.Request.CreateErrorResponse((HttpStatusCode)422, message));
+		}
+		public static HttpResponseException ExceptionNotImplemented(this ApiController apiController)
+		{
+			return new HttpResponseException(HttpStatusCode.NotImplemented);
+		}
+		public static HttpResponseException ExceptionForbidden(this ApiController apiController)
+		{
+			return new HttpResponseException(HttpStatusCode.Forbidden);
+		}
+
+		public static void ProcessEntity(this ApiController apiController, Func<string> process)
+		{
+			string error = null;
+
+			try
+			{
+				error = process();
+			}
+			catch (Exception exc)
+			{
+				error = exc.Message;
+			}
+
+			if (error.HasValue())
+				throw apiController.ExceptionUnprocessableEntity(error);
+		}
+	}
+}

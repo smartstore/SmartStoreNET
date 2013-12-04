ecab53eb5584ec7d14fd972155ac58ed203730b6
Murat Cakir <muratc@smartstore.de>
2013-12-04 17:08:05 +0100

    Added SKU to 'AskQuestion" message template

--- src/Libraries/SmartStore.Services/Installation/InvariantInstallationData.cs
+++ src/Libraries/SmartStore.Services/Installation/InvariantInstallationData.cs
@@ -4091,7 +4091,7 @@ namespace SmartStore.Services.Installation
                     {
                         Name = "Product.AskQuestion",
                         Subject = "%Store.Name% - Question concerning '%Product.Name%' from %ProductQuestion.SenderName%",
-                        Body = templateHeader + "<p>%ProductQuestion.Message%</p><p>%ProductQuestion.Message%</p><p><strong>Email:</strong> %ProductQuestion.SenderEmail%<br /><strong>Name: </strong>%ProductQuestion.SenderName%<br /><strong>Phone: </strong>%ProductQuestion.SenderPhone%</p>" + templateFooter,
+                        Body = templateHeader + "<p>%ProductQuestion.Message%</p><p>%ProductQuestion.Message%</p><p><strong>SKU:</strong> %ProductVariant.ID%<br /><strong>Email:</strong> %ProductQuestion.SenderEmail%<br /><strong>Name: </strong>%ProductQuestion.SenderName%<br /><strong>Phone: </strong>%ProductQuestion.SenderPhone%</p>" + templateFooter,
                         IsActive = true,
                         EmailAccountId = eaGeneral.Id,
                     },

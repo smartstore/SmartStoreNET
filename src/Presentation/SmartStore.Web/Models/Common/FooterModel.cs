0376d9e7d2ca699354fbbf137aab368449f9831e
Michael Herzog <herzog@smartstore.com>
2014-01-22 19:11:39 +0100

    #261 Newsletter form cannot be hidden

--- src/Presentation/SmartStore.Web/Models/Common/FooterModel.cs
+++ src/Presentation/SmartStore.Web/Models/Common/FooterModel.cs
@@ -17,6 +17,7 @@ namespace SmartStore.Web.Models.Common
         public bool ShowLegalInfo { get; set; }
         public bool ShowThemeSelector { get; set; }
         public string NewsletterEmail { get; set; }
+        public bool HideNewsletterBlock { get; set; }
         public bool BlogEnabled { get; set; }
         public bool ForumEnabled { get; set; }
         public Dictionary<string, string> Topics { get; set; }

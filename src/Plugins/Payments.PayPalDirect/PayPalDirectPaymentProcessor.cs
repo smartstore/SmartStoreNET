c4cd6a093ae327a96560cdd80c16c17ce62791bc
Marcus Gesing <home@footwear-box.de>
2014-02-20 22:45:30 +0100

    Small improvement

--- src/Plugins/Payments.PayPalDirect/PayPalDirectPaymentProcessor.cs
+++ src/Plugins/Payments.PayPalDirect/PayPalDirectPaymentProcessor.cs
@@ -135,7 +135,7 @@ namespace SmartStore.Plugin.Payments.PayPalDirect
 			var details = new DoDirectPaymentRequestDetailsType();
 			req.DoDirectPaymentRequest.DoDirectPaymentRequestDetails = details;
 			details.IPAddress = _webHelper.GetCurrentIpAddress();
-			if (details.IPAddress == "::1")
+			if (details.IPAddress == null || details.IPAddress == "::1")
 				details.IPAddress = "127.0.0.1";
 			if (authorizeOnly)
 				details.PaymentAction = PaymentActionCodeType.Authorization;

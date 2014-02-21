3cfaad6f2fb32db99b471e92e78ea938b1dd6c38
Marcus Gesing <home@footwear-box.de>
2014-02-21 16:06:40 +0100

    CommonHelper.GenerateRandomInteger was missing

--- src/Libraries/SmartStore.Core/Utilities/CommonHelper.cs
+++ src/Libraries/SmartStore.Core/Utilities/CommonHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 
 namespace SmartStore.Utilities
 {
@@ -18,5 +19,18 @@ namespace SmartStore.Utilities
                 str = String.Concat(str, random.Next(10).ToString());
             return str;
         }
+
+		/// <summary>
+		/// Returns an random interger number within a specified rage
+		/// </summary>
+		/// <param name="min">Minimum number</param>
+		/// <param name="max">Maximum number</param>
+		/// <returns>Result</returns>
+		public static int GenerateRandomInteger(int min = 0, int max = 2147483647)
+		{
+			var randomNumberBuffer = new byte[10];
+			new RNGCryptoServiceProvider().GetBytes(randomNumberBuffer);
+			return new Random(BitConverter.ToInt32(randomNumberBuffer, 0)).Next(min, max);
+		}
     }
 }

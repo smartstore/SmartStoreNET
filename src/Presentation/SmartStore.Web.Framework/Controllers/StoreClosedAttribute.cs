71e48f64be5e15249c956cab09a3205a23894d64
Marcus Gesing <home@footwear-box.de>
2014-04-19 10:50:08 +0200

    Minor changes

--- src/Presentation/SmartStore.Web.Framework/Controllers/StoreClosedAttribute.cs
+++ src/Presentation/SmartStore.Web.Framework/Controllers/StoreClosedAttribute.cs
@@ -46,7 +46,7 @@ namespace SmartStore.Web.Framework.Controllers
                 //ensure it's not the store closed page
                 !(controllerName.Equals("SmartStore.Web.Controllers.CommonController", StringComparison.InvariantCultureIgnoreCase) && actionName.Equals("StoreClosed", StringComparison.InvariantCultureIgnoreCase)) &&
 				//ensure it's not the change language page (request)
-				!(controllerName.Equals("Nop.Web.Controllers.CommonController", StringComparison.InvariantCultureIgnoreCase) && actionName.Equals("SetLanguage", StringComparison.InvariantCultureIgnoreCase)))
+				!(controllerName.Equals("SmartStore.Web.Controllers.CommonController", StringComparison.InvariantCultureIgnoreCase) && actionName.Equals("SetLanguage", StringComparison.InvariantCultureIgnoreCase)))
             {
                 if (storeInformationSettings.StoreClosedAllowForAdmins && EngineContext.Current.Resolve<IWorkContext>().CurrentCustomer.IsAdmin())
                 {

9606f0cf0e716e39693ec5431e4c976bbc5b52e6
Murat Cakir <muratc@smartstore.de>
2014-04-25 18:43:00 +0200

    Increased version no.

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -12,4 +12,4 @@
 [assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.2.0")]

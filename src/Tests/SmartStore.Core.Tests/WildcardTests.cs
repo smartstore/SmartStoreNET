33b156a91b1b964d11cce88dde19fbaff74002cd
Murat Cakir <muratc@smartstore.de>
2014-05-09 20:13:34 +0200

    Regex Wildcard fix: issues too small number ranges (<10)

---
+++ src/Tests/SmartStore.Core.Tests/WildcardTests.cs
@@ -0,0 +1,53 @@
+﻿using SmartStore.Tests;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using SmartStore.Core.Domain.Shipping;
+using SmartStore.Core.Infrastructure;
+using SmartStore.Utilities;
+
+namespace SmartStore.Core.Tests
+{
+    [TestFixture]
+    public class WildcardTests
+    {
+		
+		[Test]
+        public void Can_match_number_range()
+        {
+			var w1 = new Wildcard("999-2450");
+			Console.WriteLine(w1.Pattern);
+			Assert.IsTrue(w1.IsMatch("999"));
+			Assert.IsTrue(w1.IsMatch("1500"));
+			Assert.IsTrue(w1.IsMatch("2450"));
+			Assert.IsFalse(w1.IsMatch("500"));
+			Assert.IsFalse(w1.IsMatch("2800"));
+
+			w1 = new Wildcard("50000-59999");
+			Console.WriteLine(w1.Pattern);
+			Assert.IsTrue(w1.IsMatch("59192"));
+			Assert.IsTrue(w1.IsMatch("55000"));
+			Assert.IsFalse(w1.IsMatch("500"));
+			Assert.IsFalse(w1.IsMatch("80000"));
+
+			w1 = new Wildcard("3266-3267");
+			Console.WriteLine(w1.Pattern);
+			Assert.IsTrue(w1.IsMatch("3266"));
+			Assert.IsTrue(w1.IsMatch("3267"));
+			Assert.IsFalse(w1.IsMatch("500"));
+			Assert.IsFalse(w1.IsMatch("4000"));
+        }
+
+		[Test]
+		public void Can_match_wildcard()
+		{
+			var w1 = new Wildcard("H*o ?orld", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+			Console.WriteLine(w1.Pattern);
+			Assert.IsTrue(w1.IsMatch("Hello World"));
+			Assert.IsTrue(w1.IsMatch("hello WORLD"));
+			Assert.IsFalse(w1.IsMatch("world"));
+			Assert.IsFalse(w1.IsMatch("Hell word"));
+		}
+
+    }
+}

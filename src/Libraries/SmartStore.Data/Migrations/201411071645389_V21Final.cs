370541c928707b8a38056e7dff8570e59e4e4b0f
Murat Cakir <muratc@smartstore.de>
2014-11-07 22:51:32 +0100

    Forgot to fix some german resources

--- src/Libraries/SmartStore.Data/Migrations/201411071645389_V21Final.cs
+++ src/Libraries/SmartStore.Data/Migrations/201411071645389_V21Final.cs
@@ -50,12 +50,19 @@ namespace SmartStore.Data.Migrations
 			builder.AddOrUpdate("ShoppingCart.Totals.TaxRateLineExcl",
 				"Plus {0} % Tax",
 				"zzgl. {0} % MwSt");
-
 			builder.AddOrUpdate("Admin.Catalog.Products.Fields.IsEsd",
 				"Is Electronic Service",
 				"Ist elektronische Leistung",
 				"Specifies whether the product is an electronic service bound to EU VAT regulations for digital goods (2008/8/EG directive)",
 				"Legt fest, ob das Produkt elektronisch vertrieben wird und daher gemäß EU Richtlinie 2008/8/EG versteuert werden muss.");
+
+			// Some german fixes
+			builder.AddOrUpdate("Products.ProductHasBeenAddedToTheWishlist").Value("de", "Das Produkt wurde Ihrer Wunschliste hinzugefügt");
+			builder.AddOrUpdate("Products.ProductHasBeenAddedToTheWishlist.Link").Value("de", "Das Produkt wurde Ihrer <a href='{0}'>Wunschliste</a> hinzugefügt");
+			builder.AddOrUpdate("Products.ProductHasBeenAddedToTheCart").Value("de", "Das Produkt wurde Ihrem Warenkorb hinzugefügt");
+			builder.AddOrUpdate("Products.ProductHasBeenAddedToTheCart.Link").Value("de", "Das Produkt wurde Ihrem <a href='{0}'>Warenkorb</a> hinzugefügt");
+			builder.AddOrUpdate("Products.ProductNotAddedToTheCart.Link").Value("de", "Produkt konnte Ihrem Warenkorb nicht hinzugefügt werden.");
+			builder.AddOrUpdate("Products.RecentlyViewedProducts").Value("de", "Zuletzt angesehen");
 			
 		}
 

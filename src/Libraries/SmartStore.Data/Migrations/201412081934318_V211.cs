068fed8230a780e6f2720289bbb06121df190eac
Marcus  Gesing <home@footwear-box.de>
2014-12-11 15:57:27 +0100

    Order edit: Added hints for payment buttons

--- src/Libraries/SmartStore.Data/Migrations/201412081934318_V211.cs
+++ src/Libraries/SmartStore.Data/Migrations/201412081934318_V211.cs
@@ -30,7 +30,7 @@ namespace SmartStore.Data.Migrations
 				"Canonical host name rule",
 				"Regel für kanonischen Domänennamen");
 			builder.AddOrUpdate("Admin.Configuration.Settings.GeneralCommon.CanonicalHostNameRule.Hint",
-				"Enforces permanent redirection to a single domain name for a better page rank (e.g. myshop.com > www.myshop.com or vice versa)",
+				"Enforces permanent redirection to a single domain name for a better page rank (e.g. mystore.com > www.mystore.com or vice versa)",
 				"Erzwingt die permanente Umleitung zu einem einzelnen Domännennamen für ein besseres Seitenranking (z.B. meinshop.de > www.meinshop.de oder umgekehrt)");
 
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Seo.CanonicalHostNameRule.NoRule",
@@ -42,6 +42,41 @@ namespace SmartStore.Data.Migrations
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Seo.CanonicalHostNameRule.OmitWww",
 				"Omit www prefix",
 				"www-Präfix weglassen");
+
+			builder.AddOrUpdate("Admin.Orders.Fields.PartialRefundOffline",
+				"Partial refund (Offline)",
+				"Teilerstattung (Offline)");
+			builder.AddOrUpdate("Admin.Orders.Fields.Void",
+				"Cancel",
+				"Stornieren");
+			builder.AddOrUpdate("Admin.Orders.Fields.VoidOffline",
+				"Cancel (Offline)",
+				"Stornieren (Offline)");
+
+			builder.AddOrUpdate("Admin.Orders.Fields.MarkAsPaid.Hint",
+				"Sets the payment status to 'paid' without contacting the payment provider.",
+				"Setzt den Zahlungsstatus auf 'Bezahlt' ohne dabei den Zahlungsanbieter zu kontaktieren.");
+			builder.AddOrUpdate("Admin.Orders.Fields.Capture.Hint",
+				"Books a previously authorised payment through the payment provider.",
+				"Zieht eine zuvor reservierte Zahlung über den Zahlungsanbieter ein.");
+			builder.AddOrUpdate("Admin.Orders.Fields.Refund.Hint",
+				"Initiates a refund of the total order value at the payment provider.",
+				"Leitet eine Rückerstattung des gesamten Auftragswertes beim Zahlungsanbieter ein.");
+			builder.AddOrUpdate("Admin.Orders.Fields.RefundOffline.Hint",
+				"Setzt the payment status to 'refunded' without contacting the payment provider.",
+				"Setzt den Zahlungsstatus auf 'Erstattet', ohne dabei den Zahlungsanbieter zu kontaktieren.");
+			builder.AddOrUpdate("Admin.Orders.Fields.PartialRefund.Hint",
+				"Initiates the refund of a partial amount of the total order value at the payment provider.",
+				"Leitet die Rückerstattung eines Teilbetrages des Auftragswertes beim Zahlungsanbieter ein.");
+			builder.AddOrUpdate("Admin.Orders.Fields.PartialRefundOffline.Hint",
+				"Setzt the payment status to 'partially refunded' including the refunded amount without contacting the payment provider.",
+				"Setzt den Zahlungsstatus auf 'Teilweise erstattet' samt Erstattungsbetrag, ohne dabei den Zahlungsanbieter zu kontaktieren.");
+			builder.AddOrUpdate("Admin.Orders.Fields.Void.Hint",
+				"Initiates the cancellation of the payment transaction at the payment provider.",
+				"Leitet die Stornierung der Zahlungstransaktion beim Zahlungsanbieter ein.");
+			builder.AddOrUpdate("Admin.Orders.Fields.VoidOffline.Hint",
+				"Setzt the payment status to 'canceled' without contacting the payment provider.",
+				"Setzt den Zahlungsstatus auf 'Storniert', ohne dabei den Zahlungsanbieter zu kontaktieren.");
 		}
     }
 }

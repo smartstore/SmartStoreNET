0f82ea7edfc34725229c8c7784b12cf01a8f8e51
Marcus  Gesing <home@footwear-box.de>
2015-02-26 20:18:54 +0100

    Removed HasSingleLicenseForAllVersions option.

--- src/Libraries/SmartStore.Core/Plugins/ILicensable.cs
+++ src/Libraries/SmartStore.Core/Plugins/ILicensable.cs
@@ -3,7 +3,7 @@ namespace SmartStore.Core.Plugins
 {
 	/// <summary>
 	/// Marks a plugin as a licensed piece of code where the user has to enter a license key that has to be activated.
-	/// Note that a license key is only valid for one IP address (the one used to activate the key).
+	/// Note that a license key is only valid for the IP address that activated the key.
 	/// </summary>
 	public interface ILicensable
 	{
@@ -11,10 +11,5 @@ namespace SmartStore.Core.Plugins
 		/// Whether one license (key) is valid for all stores. Otherwise a new key is required for each store.
 		/// </summary>
 		bool HasSingleLicenseForAllStores { get; }
-
-		/// <summary>
-		/// Whether one license (key) is valid for all plugin versions. Otherwise a new key is required if the major version changes.
-		/// </summary>
-		bool HasSingleLicenseForAllVersions { get; }
 	}
 }

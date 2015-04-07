2879beef2f2fb32346d963167b96250431953c9c
Marcus Gesing <home@footwear-box.de>
2015-04-07 22:12:31 +0200

    Resolves #670 Move FileDownloadManager to SmartStore.Core.Utilities

--- src/Libraries/SmartStore.Core/Utilities/FileDownloadManager.cs
+++ src/Libraries/SmartStore.Core/Utilities/FileDownloadManager.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using SmartStore.Core.Logging;
 
-namespace SmartStore.Web.Framework.Utilities
+namespace SmartStore.Utilities
 {
 	public class FileDownloadManager
 	{

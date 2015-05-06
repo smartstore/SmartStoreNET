bb0a6b9eaf8c2e72428230119af6e6b3af0fbbbf
Murat Cakir <muratc@smartstore.de>
2015-05-06 21:53:56 +0200

    Increased version number to 2.2.1

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -12,4 +12,4 @@
 [assembly: AssemblyVersion("2.2.0.0")]
 
 [assembly: AssemblyFileVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.1.0")]

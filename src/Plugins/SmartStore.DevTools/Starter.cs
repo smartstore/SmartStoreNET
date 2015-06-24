7eb8b8de05cbe12b18a824f4126b637687416ba5
Murat Cakir <muratc@smartstore.de>
2015-06-24 22:22:30 +0200

    Added EF6 profiling to MiniProfiler

---
+++ src/Plugins/SmartStore.DevTools/Starter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using SmartStore.Core.Infrastructure;
+using SmartStore.Core.Plugins;
+using SmartStore.Web.Framework;
+
+namespace SmartStore.DevTools
+{
+	
+	public class ProfilerPreApplicationStart : IPreApplicationStart
+	{
+		public void Start()
+		{
+			DynamicModuleUtility.RegisterModule(typeof(ProfilerHttpModule));
+			SmartUrlRoutingModule.RegisterRoutablePath("/mini-profiler-resources/(.*)");
+		}
+	}
+
+	public class ProfilerStartupTask : IStartupTask
+	{
+		public void Execute()
+		{
+			StackExchange.Profiling.EntityFramework6.MiniProfilerEF6.Initialize();
+		}
+
+		public int Order
+		{
+			get { return int.MinValue; }
+		}
+	}
+
+}

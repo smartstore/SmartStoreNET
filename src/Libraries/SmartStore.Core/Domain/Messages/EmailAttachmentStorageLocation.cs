af28c9bc56b68f800c0f64c2f485c312bdb3c812
Marcus  Gesing <home@footwear-box.de>
2015-07-14 00:46:19 +0200

    Solution did not compile: Included missing files and code. Re-created QueuedEmailAttachments migration.

---
+++ src/Libraries/SmartStore.Core/Domain/Messages/EmailAttachmentStorageLocation.cs
@@ -0,0 +1,21 @@
+﻿
+namespace SmartStore.Core.Domain.Messages
+{
+	public enum EmailAttachmentStorageLocation
+	{
+		/// <summary>
+		/// Attachment is embedded as Blob
+		/// </summary>
+		Blob,
+
+		/// <summary>
+		/// Attachment is a reference to <see cref="Download"/>
+		/// </summary>
+		FileReference,
+		
+		/// <summary>
+		/// Attachment is located on disk (physical or virtual path)
+		/// </summary>
+		Path
+	}
+}
--- src/Libraries/SmartStore.Core/Domain/Messages/QueuedEmailAttachment.cs
+++ src/Libraries/SmartStore.Core/Domain/Messages/QueuedEmailAttachment.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SmartStore.Core.Domain.Media;
+﻿using SmartStore.Core.Domain.Media;
 
 namespace SmartStore.Core.Domain.Messages
 {

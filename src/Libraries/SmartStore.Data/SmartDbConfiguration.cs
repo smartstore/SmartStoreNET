0961e3dbf2ccc1899901d8a1b1b6129e4784b707
Murat Cakir <muratc@smartstore.de>
2015-07-16 00:11:44 +0200

    Implemented Entity Framework 2nd level cache for some unfrequently changed entities

--- src/Libraries/SmartStore.Data/SmartDbConfiguration.cs
+++ src/Libraries/SmartStore.Data/SmartDbConfiguration.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Infrastructure.DependencyResolution;
 using System.Linq;
+using EFCache;
 using SmartStore.Core.Data;
+using SmartStore.Core.Infrastructure;
 using SmartStore.Data.Setup;
+using SmartStore.Data.Caching;
 
 namespace SmartStore.Data
 {
@@ -24,6 +28,26 @@ namespace SmartStore.Data
 			if (provider != null)
 			{
 				base.SetDefaultConnectionFactory(provider.GetConnectionFactory());
+
+				// prepare EntityFramework 2nd level cache
+				ICache cache = null;
+				try
+				{
+					var innerCache = EngineContext.Current.Resolve<Func<Type, SmartStore.Core.Caching.ICache>>();
+					cache = new EfCacheImpl(innerCache(typeof(SmartStore.Core.Caching.StaticCache)));
+				}
+				catch
+				{
+					cache = new InMemoryCache();
+				}
+
+				var transactionHandler = new CacheTransactionHandler(cache);
+				AddInterceptor(transactionHandler);
+
+				Loaded +=
+				  (sender, args) => args.ReplaceService<DbProviderServices>(
+					(s, _) => new CachingProviderServices(s, transactionHandler,
+					  new EfCachingPolicy()));
 			}
 		}
 	}

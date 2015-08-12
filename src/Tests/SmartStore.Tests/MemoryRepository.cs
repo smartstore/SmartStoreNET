13d91a71ef98d3da1ce076f2dde3830b27e08047
Murat Cakir <muratc@smartstore.de>
2015-08-12 20:57:34 +0200

    Implementing test cases

--- src/Tests/SmartStore.Tests/MemoryRepository.cs
+++ src/Tests/SmartStore.Tests/MemoryRepository.cs
@@ -14,12 +14,12 @@ namespace SmartStore.Tests
 {
 	public class MemoryRepository<T> : IRepository<T> where T : BaseEntity, new()
 	{
-		private readonly IDictionary<int, T> _data = new Dictionary<int, T>();
+		private readonly TestDbSet<T> _dbSet = new TestDbSet<T>();
 		private IDbContext _dbContext;
 
 		public IQueryable<T> Table
 		{
-			get { return _data.Values.AsQueryable(); }
+			get { return _dbSet; }
 		}
 
 		public IQueryable<T> TableUntracked
@@ -29,54 +29,51 @@ namespace SmartStore.Tests
 
 		public ICollection<T> Local
 		{
-			get { return _data.Values; }
+			get { return _dbSet.Local; }
 		}
 
 		public T Create()
 		{
-			return Activator.CreateInstance<T>();
+			return _dbSet.Create<T>();
 		}
 
 		public T GetById(object id)
 		{
-			int id2 = (int)id;
-			T entity;
-			if (_data.TryGetValue(id2, out entity))
-			{
-				return entity;
-			}
-
-			return null;
+			return _dbSet.Find(id);
 		}
 
 		public void Insert(T entity)
 		{
-			_data[entity.Id] = entity;
+			if (entity.Id <= 0)
+			{
+				entity.Id = Local.Count == 0 ? 1 : Local.Max(x => x.Id) + 1;
+			}
+			_dbSet.Add(entity);
 		}
 
 		public void InsertRange(IEnumerable<T> entities, int batchSize = 100)
 		{
-			entities.Each(x => _data[x.Id] = x);
+			entities.Each(x => Insert(x));
 		}
 
 		public void Update(T entity)
 		{
-			_data[entity.Id] = entity;
+			// Noop
 		}
 
 		public void UpdateRange(IEnumerable<T> entities)
 		{
-			entities.Each(x => _data[x.Id] = x);
+			entities.Each(x => Update(x));
 		}
 
 		public void Delete(T entity)
 		{
-			_data.Remove(entity.Id);
+			_dbSet.Remove(entity);
 		}
 
 		public void DeleteRange(IEnumerable<T> entities)
 		{
-			entities.Each(x => Delete(x));
+			_dbSet.RemoveRange(entities);
 		}
 
 		public IQueryable<T> Expand(IQueryable<T> query, string path)
@@ -105,18 +102,14 @@ namespace SmartStore.Tests
 			{
 				if (_dbContext == null)
 				{
-					var dbSet = MockRepository.GenerateMock<DbSet<T>>();
-					dbSet.Expect(x => ((IQueryable<T>)x).Provider).Return(_data.Values.AsQueryable().Provider);
-					dbSet.Expect(x => ((IQueryable<T>)x).Expression).Return(_data.Values.AsQueryable().Expression);
-					dbSet.Expect(x => ((IQueryable<T>)x).ElementType).Return(_data.Values.AsQueryable().ElementType);
-					dbSet.Expect(x => ((IQueryable<T>)x).GetEnumerator()).Return(_data.Values.AsQueryable().GetEnumerator());
-					
-					_dbContext = MockRepository.GenerateMock<IDbContext>();
-					_dbContext.Expect(x => x.Set<T>()).Return(dbSet);
-					_dbContext.Expect(x => x.SaveChangesAsync()).Return(Task.FromResult<int>(0));
-					_dbContext.Expect(x => x.ExecuteStoredProcedureList<T>(Arg<string>.Is.Anything)).Return(new List<T>());
-					_dbContext.Expect(x => x.GetModifiedProperties(Arg<BaseEntity>.Is.Anything)).Return(new Dictionary<string, object>());
-					_dbContext.Expect(x => x.BeginTransaction(Arg<IsolationLevel>.Is.Anything)).Return(MockRepository.GenerateMock<ITransaction>());
+					var ctx = MockRepository.GenerateMock<IDbContext>();
+					ctx.Stub(x => x.Set<T>()).Return(_dbSet);
+					ctx.Stub(x => x.SaveChangesAsync()).Return(Task.FromResult<int>(0));
+					ctx.Stub(x => x.ExecuteStoredProcedureList<T>(Arg<string>.Is.Anything, Arg<object[]>.Is.Anything)).Return(new List<T>());
+					ctx.Stub(x => x.GetModifiedProperties(Arg<BaseEntity>.Is.Anything)).Return(new Dictionary<string, object>());
+					ctx.Stub(x => x.BeginTransaction(Arg<IsolationLevel>.Is.Anything)).Return(MockRepository.GenerateMock<ITransaction>());
+
+					_dbContext = ctx;
 				}
 
 				return _dbContext;

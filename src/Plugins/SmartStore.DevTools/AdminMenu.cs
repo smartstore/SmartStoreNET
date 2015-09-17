7ed204162f6ddbadbd49869ba35a16ac122cd973
Marcus Gesing <home@footwear-box.de>
2015-09-17 21:38:32 +0200

    Dev tools: Added an example of how a plugin can integrate a page into the backend

--- src/Plugins/SmartStore.DevTools/AdminMenu.cs
+++ src/Plugins/SmartStore.DevTools/AdminMenu.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Routing;
-using System.Web.Mvc;
+﻿using SmartStore.Collections;
 using SmartStore.Web.Framework.UI;
-using SmartStore.Collections;
 
 namespace SmartStore.DevTools
 {
@@ -20,6 +14,15 @@ namespace SmartStore.DevTools
 				.ToItem();
 
 			pluginsNode.Prepend(menuItem);
+
+			// uncomment to add to admin menu (see plugin sub-menu)
+			//var backendExtensionItem = new MenuItem().ToBuilder()
+			//	.Text("Backend extension")
+			//	.Icon("area-chart")
+			//	.Action("BackendExtension", "DevTools", new { area = "SmartStore.DevTools" })
+			//	.ToItem();
+
+			//pluginsNode.Append(backendExtensionItem);
 		}
 	}
 }

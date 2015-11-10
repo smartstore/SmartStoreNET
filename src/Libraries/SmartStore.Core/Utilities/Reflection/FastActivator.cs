ebd50823c859e126493a52bf2b723b6b9e359681
Murat Cakir <muratc@smartstore.de>
2015-11-10 17:07:58 +0100

    Added FastActivator helper class

---
+++ src/Libraries/SmartStore.Core/Utilities/Reflection/FastActivator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SmartStore.Utilities.Reflection
+{
+	public class FastActivator
+	{
+		private Action<object[], object> _invoker;
+
+		public FastActivator(ConstructorInfo constructorInfo)
+		{
+			Guard.ArgumentNotNull(() => constructorInfo);
+
+			ConstructorInfo = constructorInfo;
+			Invoker = MakeFastInvoker(constructorInfo);
+		}
+
+		/// <summary>
+		/// Gets the backing <see cref="ConstructorInfo"/>.
+		/// </summary>
+		public ConstructorInfo ConstructorInfo { get; }
+
+		/// <summary>
+		/// Gets the constructor invoker.
+		/// </summary>
+		public Func<object[], object> Invoker { get; }
+
+		/// <summary>
+		/// Creates an instance of the type using the specified parameters.
+		/// </summary>
+		/// <returns>A reference to the newly created object.</returns>
+		public object Activate(params object[] parameters)
+		{
+			return Invoker(parameters);
+		}
+
+		/// <summary>
+		/// Creates a single fast constructor invoker. The result is not cached.
+		/// </summary>
+		/// <param name="constructorInfo">constructorInfo to create invoker for.</param>
+		/// <returns>a fast invoker.</returns>
+		public static Func<object[], object> MakeFastInvoker(ConstructorInfo constructorInfo)
+		{
+			// parameters to execute
+			var parametersParameter = Expression.Parameter(typeof(object[]), "parameters");
+
+			// build parameter list
+			var parameterExpressions = new List<Expression>();
+			var paramInfos = constructorInfo.GetParameters();
+			for (int i = 0; i < paramInfos.Length; i++)
+			{
+				var valueObj = Expression.ArrayIndex(parametersParameter, Expression.Constant(i));
+				var valueCast = Expression.Convert(valueObj, paramInfos[i].ParameterType);
+
+				parameterExpressions.Add(valueCast);
+			}
+
+			// new T((T0)parameters[0], (T1)parameters[1], ...)
+			var instanceCreate = Expression.New(constructorInfo, parameterExpressions);
+
+			// (object)new T((T0)parameters[0], (T1)parameters[1], ...)
+			var instanceCreateCast = Expression.Convert(instanceCreate, typeof(object));
+
+			var lambda = Expression.Lambda<Func<object[], object>>(instanceCreateCast, parametersParameter);
+
+			return lambda.Compile();
+		}
+	}
+}
--- src/Tests/SmartStore.Core.Tests/PerformanceTests.cs
+++ src/Tests/SmartStore.Core.Tests/PerformanceTests.cs
@@ -1,9 +1,11 @@
-﻿using SmartStore.Tests;
-using NUnit.Framework;
-using System.Diagnostics;
-using System;
-using SmartStore.Core.Domain.Catalog;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using NUnit.Framework;
+using SmartStore.Core.Domain.Catalog;
+using SmartStore.Tests;
+using SmartStore.Utilities.Reflection;
 
 namespace SmartStore.Core.Tests
 {
@@ -13,15 +15,19 @@ namespace SmartStore.Core.Tests
 		[Test]
 		public void InstantiatePerfTest()
 		{
-			int cycles = 1000000;
+			int cycles = 500000;
 
 			Chronometer.Measure(cycles, "Create Product NATIVE", i => new Product());
-			Chronometer.Measure(cycles, "Create Product Reflection", i => Activator.CreateInstance<Product>());
+			Chronometer.Measure(cycles, "Create Product Reflection", i => Activator.CreateInstance(typeof(Product)));
 
 			var list = new List<Product>();
 
 			Chronometer.Measure(cycles, "Create List<Product> NATIVE", i => new List<Product>(list));
 			Chronometer.Measure(cycles, "Create List<Product> Reflection", i => Activator.CreateInstance(typeof(List<Product>), list));
+
+			var ctor = typeof(List<Product>).GetConstructor(new Type[] { typeof(List<Product>) });
+			var activator = new FastActivator(ctor);
+			Chronometer.Measure(cycles, "Create List<Product> FASTACTIVATOR", i => activator.Activate(list) );
 		}
 	}
 }

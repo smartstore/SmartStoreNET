42cc92cdf97d837b73eea2ff194b8afd65c20916
Murat Cakir <muratc@smartstore.de>
2016-03-10 22:47:45 +0100

    Made LocalizedEntity compound index unique

--- src/Libraries/SmartStore.Core/Domain/Localization/LocalizedProperty.cs
+++ src/Libraries/SmartStore.Core/Domain/Localization/LocalizedProperty.cs
@@ -13,28 +13,28 @@ namespace SmartStore.Core.Domain.Localization
         /// Gets or sets the entity identifier
         /// </summary>
 		[DataMember]
-		[Index("IX_LocalizedProperty_Compound", 1)]
+		[Index("IX_LocalizedProperty_Compound", Order = 1, IsUnique = true)]
 		public int EntityId { get; set; }
 
         /// <summary>
         /// Gets or sets the language identifier
         /// </summary>
 		[DataMember]
-		[Index("IX_LocalizedProperty_Compound", 4)]
+		[Index("IX_LocalizedProperty_Compound", Order = 4, IsUnique = true)]
 		public int LanguageId { get; set; }
 
         /// <summary>
         /// Gets or sets the locale key group
         /// </summary>
 		[DataMember]
-		[Index("IX_LocalizedProperty_Compound", 3)]
+		[Index("IX_LocalizedProperty_Compound", Order = 3, IsUnique = true)]
 		public string LocaleKeyGroup { get; set; }
 
         /// <summary>
         /// Gets or sets the locale key
         /// </summary>
 		[DataMember]
-		[Index("IX_LocalizedProperty_Compound", 2)]
+		[Index("IX_LocalizedProperty_Compound", Order = 2, IsUnique = true)]
 		public string LocaleKey { get; set; }
 
         /// <summary>

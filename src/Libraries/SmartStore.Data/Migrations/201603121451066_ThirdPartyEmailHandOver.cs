717c30804a4c4e8a28e5b955964dbae5c5917e71
Murat Cakir <muratc@smartstore.de>
2016-04-15 01:46:34 +0200

    Several language resource fixes

--- src/Libraries/SmartStore.Data/Migrations/201603121451066_ThirdPartyEmailHandOver.cs
+++ src/Libraries/SmartStore.Data/Migrations/201603121451066_ThirdPartyEmailHandOver.cs
@@ -48,25 +48,25 @@ namespace SmartStore.Data.Migrations
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Orders.CheckoutThirdPartyEmailHandOver.Activated", "Show activated", "Aktiviert anzeigen");
 
 			builder.AddOrUpdate("Admin.Configuration.Settings.ShoppingCart.ThirdPartyEmailHandOver",
-				"Third party email hand over",
-				"E-Mail Weitergabe an Dritte",
-				"Specifies if customers can accept to hand over their email address to third party when ordering and if the checkbox is enabled by default.",
-				"Legt fest, ob Kunden bei einer Bestellung der Weitergabe Ihrer E-Mail Adresse an Dritte zustimmen können und ob die Checkbox standardmäßig aktiviert ist.");
+				"Consent for email transfer to third parties",
+				"Zustimmung zur E-Mail Weitergabe an Dritte",
+				"Specifies whether customers can agree to a transferring of their email address to third parties when ordering, and whether the checkbox is enabled by default during checkout.",
+				"Legt fest, ob Kunden bei einer Bestellung der Weitergabe ihrer E-Mail Adresse an Dritte zustimmen können und ob die Checkbox dafür standardmäßig aktiviert ist.");
 
 			builder.AddOrUpdate("Admin.Configuration.Settings.ShoppingCart.ThirdPartyEmailHandOverLabel",
-				"Text for email hand over",
+				"Text for email transfer consent",
 				"Text für E-Mail Weitergabe",
-				"Specifies the text to accept to hand over the email address to third party. Please choose a certain reason for the hand over, e.g. 'I agree with the transmission and storage of my email address for the trusted shops buyer protection.'",
+				"Specifies the text to be displayed to the customer. Please choose a specific reason, e.g. 'I agree to the transfer and storage of my email address for TrustedShops buyer protection.'",
 				"Legt den Text für die Zustimmung zur Weitergabe der E-Mail Adresse an Dritte fest. Wählen Sie bitte einen konkreten Grund für die Weitergabe, z.B. 'Mit der Übermittlung und Speicherung meiner E-Mail-Adresse zur Abwicklung des Käuferschutzes durch Trusted Shops bin ich einverstanden.'");
 
 			builder.AddOrUpdate("Admin.Configuration.Settings.ShoppingCart.ThirdPartyEmailHandOverLabel.Default",
-				"I agree with the transmission and storage of my email address by third parties.",
+				"I agree to the transfer and storage of my email address by third parties.",
 				"Mit der Übermittlung und Speicherung meiner E-Mail-Adresse durch dritte Parteien bin ich einverstanden.");
 
 			builder.AddOrUpdate("Admin.Orders.Fields.AcceptThirdPartyEmailHandOver",
-				"Accepts hand over of email",
+				"Accepts transfer of email",
 				"Akzeptiert Weitergabe der E-Mail",
-				"Indicates whether the customer has accepted to hand over his email address to third party.",
+				"Indicates whether the customer has agreed to a transfer of his email address to third parties.",
 				"Gibt an, ob der Kunde bei der Bestellung einer Weitergabe seiner E-Mail Adresse an Dritte zugestimmt hat oder nicht.");
 
 			builder.AddOrUpdate("Admin.OrderNotice.OrderCaptureError",
@@ -89,17 +89,17 @@ namespace SmartStore.Data.Migrations
 				"Legt fest, ob das Standardbild bei Herstellern ausgeblendet werden soll. Das Standardbild wird angezeigt, wenn dem Hersteller kein Bild zugeordnet ist.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Partition.Note",
-				"You can partition the data to be exported with the following settings. This includes<ul><li>Skipping the first n records</li><li>The maximum number of records to be exported</li><li>The number of records per export file</li><li>Export data for each shop in a separate file</li></ul>By default, all data of a store will be exported into one file.",
+				"With the following settings you can partition the data to be exported. This includes<ul><li>Skipping the first n records</li><li>The maximum number of records to be exported</li><li>The number of records per export file</li><li>Export data for each shop in a separate file</li></ul>By default, all data of a store will be exported into one file.",
 				"Mit den folgenden Einstellungen lassen sich die zu exportierenden Daten aufteilen. Dazu zählt<ul><li>Das Überspringen der ersten n Datensätze</li><li>Die maximale Anzahl zu exportierender Datensätze</li><li>Die Anzahl der Datensätze pro Exportdatei</li><li>Daten von jedem Shop in eine separate Datei exportieren</li></ul>Standardmäßig werden alle Daten eines Shops in eine Datei exportiert.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Partition.Validate",
-				"Partitioning settings must be greater than or equal to 0.",
+				"Partitioning setting values must be greater than or equal to 0.",
 				"Einstellungen zur Aufteilung müssen größer oder gleich 0 sein.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Filter.IsActiveSubscriber",
-				"Only active subscriber",
+				"Only active subscribers",
 				"Nur aktive Abonnenten",
-				"Filter by active or inactive newsletter subscriber.",
+				"Filter by active or inactive newsletter subscribers.",
 				"Nach aktiven bzw. inaktiven Newsletter Abonnenten filtern.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Filter.IsActiveCustomer",
@@ -133,19 +133,19 @@ namespace SmartStore.Data.Migrations
 				"Nach dem Datum der letzten Shop Aktivität filtern.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Filter.LastActivityTo",
-				"Last activity to",
+				"Last active until",
 				"Zuletzt aktiv bis",
 				"Filter by date of last store activity.",
-				"Nach dem Datum der letzten Shop Aktivität filtern.");
+				"Nach dem Datum der letzten Shop-Aktivität filtern.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Filter.HasSpentAtLeastAmount",
-				"Has spent x amount",
+				"Has spent amount x",
 				"Hat Betrag x ausgegeben",
 				"Filter by spent amount.",
 				"Nach dem insgesamt ausgegebenen Betrag filtern.");
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.Filter.HasPlacedAtLeastOrders",
-				"Has x placed orders",
+				"Has placed x orders",
 				"Hat x Bestellungen",
 				"Filter by number of placed orders.",
 				"Nach der Anzahl der getätigten Bestellungen filtern.");
@@ -165,13 +165,13 @@ namespace SmartStore.Data.Migrations
 			builder.Delete("Admin.DataExchange.Import.ProfileEntitySelectNote");
 
 			builder.AddOrUpdate("Admin.Configuration.Restriction.SaveBeforeEdit",
-				"You need to save the data before you can specify restrictions.",
+				"You need to save before you can specify restrictions.",
 				"Sie müssen zunächst speichern, bevor Sie Einschränkungen festlegen können.");
 
 			builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.CustomerNumberMethod",
 				"Customer numbers",
 				"Kundennummern",
-				"Specifies whether to assign customer numbers and whether these should be created automatically.",
+				"Specifies whether to assign customer numbers and whether they should be created automatically.",
 				"Legt fest, ob Kundennummern vergeben werden und ob diese automatisch vergeben werden sollen.");
 
 			builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.CustomerNumberVisibility",
@@ -187,11 +187,11 @@ namespace SmartStore.Data.Migrations
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Customers.CustomerNumberVisibility.None",	"Do not display", "Nicht anzeigen");
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Customers.CustomerNumberVisibility.Display", "Display", "Anzeigen");
 			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Customers.CustomerNumberVisibility.EditableIfEmpty", "Editable if empty", "Editierbar falls leer");
-			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Customers.CustomerNumberVisibility.Editable", "Always editable", "Stets editierbar");
+			builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Customers.CustomerNumberVisibility.Editable", "Always editable", "Immer editierbar");
 
 			builder.AddOrUpdate("Admin.Common.FileInUse",
 				"The file is in use and cannot be opened.",
-				"Die Datei ist in Benutzung und kann nicht geöffnet werden.");
+				"Die Datei ist in Benutzung und kann daher nicht geöffnet werden.");
 
 
 			builder.AddOrUpdate("Admin.DataExchange.Export.UserProfilesTitle",

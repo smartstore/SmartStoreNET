7ae13e567fc3d95c2d5a8235ced9248e9e11d3fb
Murat Cakir <muratc@smartstore.de>
2016-04-19 00:06:11 +0200

    Added Sass && SCSS translator

---
+++ src/Presentation/SmartStore.Web.Framework/Theming/HttpHandlers/SassCssHttpHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using BundleTransformer.Core.Assets;
+using BundleTransformer.Core.Transformers;
+using BundleTransformer.SassAndScss.Translators;
+
+namespace SmartStore.Web.Framework.Theming
+{
+	public class SassCssHttpHandler : CssHttpHandlerBase
+	{
+		protected override IAsset TranslateAssetCore(IAsset asset, ITransformer transformer, bool isDebugMode)
+		{
+			return InnerTranslateAsset<SassAndScssTranslator>("SassAndScssTranslator", asset, transformer, isDebugMode);
+		}
+	}
+}

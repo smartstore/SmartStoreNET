459986ab7b73b543852db458b4b208c2cdeeb4df
Murat Cakir <muratc@smartstore.de>
2016-09-27 17:05:44 +0200

    Search: minor addition

--- src/Libraries/SmartStore.Services/Search/CatalogSearchResult.cs
+++ src/Libraries/SmartStore.Services/Search/CatalogSearchResult.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SmartStore.Core;
 using SmartStore.Core.Domain.Catalog;
+using SmartStore.Core.Search;
 
 namespace SmartStore.Services.Search
 {
 	public partial class CatalogSearchResult
 	{
 		IPagedList<Product> Hits { get; }
+
+		/// <summary>
+		/// The original catalog search query
+		/// </summary>
+		CatalogSearchQuery Query { get; }
+
+		/// <summary>
+		/// Gets the word suggestions.
+		/// </summary>
+		string[] Suggestions { get; }
 
 		// TODO: Facets etc.
 	}

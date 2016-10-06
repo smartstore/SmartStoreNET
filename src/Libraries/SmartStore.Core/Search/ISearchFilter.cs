bc0e277fa01a7509304d1dd6ddcf043ce5359878
Murat Cakir <muratc@smartstore.de>
2016-10-06 19:25:55 +0200

    Search: new interfaces for filtering

---
+++ src/Libraries/SmartStore.Core/Search/ISearchFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SmartStore.Core.Search
+{
+	public interface ISearchFilter
+	{
+		SearchFilterOccurence Occurence { get; }
+		float Boost { get; }
+	}
+
+	public interface ICompositeSearchFilter : ISearchFilter
+	{
+		ISearchFilter[] Filters { get; }
+	}
+
+	public interface ITermSearchFilter : ISearchFilter
+	{
+		string FieldName { get; }
+		IndexTypeCode TypeCode { get; }
+		object Term { get; }
+		bool IsExactMatch { get; }
+		bool IsNotAnalyzed { get; }
+	}
+
+	public interface IRangeSearchFilter : ITermSearchFilter
+	{
+		object UpperTerm { get; }
+		bool IncludesLower { get; }
+		bool IncludesUpper { get; }
+	}
+}

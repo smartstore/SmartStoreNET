781a487b87d951d3649a1d699d5f88e880c1dbaa
Murat Cakir <muratc@smartstore.de>
2016-11-03 01:44:13 +0100

    Minor refactoring

---
+++ src/Libraries/SmartStore.Core/Search/Facets/FacetValue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SmartStore.Core.Search.Facets
+{
+	[Serializable]
+	public class FacetValue
+	{
+		public FacetValue(bool value)
+			: this(value, IndexTypeCode.Boolean)
+		{
+		}
+
+		public FacetValue(int value)
+			: this(value, IndexTypeCode.Int32)
+		{
+		}
+
+		public FacetValue(double value)
+			: this(value, IndexTypeCode.Double)
+		{
+		}
+
+		public FacetValue(DateTime value)
+			: this(value, IndexTypeCode.DateTime)
+		{
+		}
+
+		public FacetValue(string value)
+			: this(value, IndexTypeCode.String)
+		{
+		}
+
+		public FacetValue(object value, IndexTypeCode typeCode)
+		{
+			Value = value;
+			TypeCode = typeCode;
+		}
+
+		public object Value
+		{
+			get;
+			private set;
+		}
+
+		public IndexTypeCode TypeCode
+		{
+			get;
+			private set;
+		}
+	}
+}

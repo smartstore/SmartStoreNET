40856936bd1c03b14d5a95d960d08ff98d141ac1
Murat Cakir <muratc@smartstore.de>
2016-12-01 18:13:55 +0100

    Theming: product list

---
+++ src/Presentation/SmartStore.Web/Themes/Alpha/Scripts/smartstore.articlelist.js
@@ -0,0 +1,16 @@
+﻿/*
+*  Project: SmartStore Article List
+*  Author: Murat Cakir, SmartStore AG
+*/
+
+; (function ($, window, document, undefined) {
+
+	$('.artlist-style-grid').on('mouseenter', '.art', function (e) {
+		console.log("ENTER", e);
+	});
+
+	$('.artlist-style-grid').on('mouseleave', '.art', function (e) {
+		console.log("LEAVE", e);
+	});
+
+})(jQuery, window, document);

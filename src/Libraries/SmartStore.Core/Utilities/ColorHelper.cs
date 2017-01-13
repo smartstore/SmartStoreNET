e67fbb0eb7894826db35a01c16128017d0eb1262
Murat Cakir <muratc@smartstore.de>
2017-01-13 20:00:40 +0100

    Added ColorHelper.GetPerceivedBrightess()

---
+++ src/Libraries/SmartStore.Core/Utilities/ColorHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace SmartStore.Utilities
+{
+	public static class ColorHelper
+	{
+		public static int GetPerceivedBrightness(string htmlColor)
+		{
+			Guard.NotEmpty(htmlColor, nameof(htmlColor));
+
+			return GetPerceivedBrightness(ColorTranslator.FromHtml(htmlColor));
+		}
+
+		/// <summary>
+		/// Calculates the perceived brightness of a color.
+		/// </summary>
+		/// <param name="color">The color</param>
+		/// <returns>
+		/// A number in the rage of 0 (black) to 255 (White). 
+		/// For text contrast colors, an optimal cutoff value is 130.
+		/// </returns>
+		public static int GetPerceivedBrightness(Color color)
+		{
+			return (int)Math.Sqrt(
+			   color.R * color.R * .241 +
+			   color.G * color.G * .691 +
+			   color.B * color.B * .068);
+		}
+	}
+}

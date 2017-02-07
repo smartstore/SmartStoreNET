3bd76924ea7546f343f28689ae4660e93bd8bac5
Michael Herzog <herzog@smartstore.com>
2017-02-07 18:59:17 +0100

    Altered last commit & included FontAwesome into CKEditor

--- src/Presentation/SmartStore.Web/Content/editors/ckeditor/config.js
+++ src/Presentation/SmartStore.Web/Content/editors/ckeditor/config.js
@@ -64,9 +64,7 @@ CKEDITOR.editorConfig = function( config ) {
     config.qtCellSpacing = '0'; // Cell spacing of the inserted table (default: '1')
 
     config.removePlugins = 'placeholder';
-
-    config.contentsCss = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css";
-		
+	
 	config.codemirror = {
 	    // Set this to the theme you wish to use (codemirror themes)
 	    theme: 'eclipse',

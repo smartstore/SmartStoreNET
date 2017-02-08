ef615745cc05dca4112009eadf4bac35153200e1
Marcus Gesing <home@footwear-box.de>
2017-02-08 10:50:40 +0100

    Search: More on attribute alias mapping

--- src/Libraries/SmartStore.Services/Search/Modelling/CatalogSearchQueryAliasMapper.cs
+++ src/Libraries/SmartStore.Services/Search/Modelling/CatalogSearchQueryAliasMapper.cs
@@ -10,7 +10,8 @@ namespace SmartStore.Services.Search.Modelling
 {
 	public class CatalogSearchQueryAliasMapper : ICatalogSearchQueryAliasMapper
 	{
-		private const string ALL_MAPPINGS_KEY = "search.alias.mappings.all";
+		private const string ALL_ID_BY_ALIAS_MAPPINGS_KEY = "search.id.alias.mappings.all";
+		private const string ALL_ALIAS_BY_ID_MAPPINGS_KEY = "search.alias.id.mappings.all";
 
 		private readonly ICacheManager _cacheManager;
 		private readonly ISpecificationAttributeService _specificationAttributeService;
@@ -30,15 +31,23 @@ namespace SmartStore.Services.Search.Modelling
 		{
 			return $"attr.{languageId}.{attributeAlias}";
 		}
+		protected string CreateAttributeKey(int languageId, int attributeId)
+		{
+			return $"attr.{languageId}.{attributeId}";
+		}
 
 		protected string CreateOptionKey(int languageId, int attributeId, string optionAlias)
 		{
 			return $"attr.option.{languageId}.{attributeId}.{optionAlias}";
 		}
-
-		protected virtual IDictionary<string, int> GetAllCachedMappings()
+		protected string CreateOptionKey(int languageId, int optionId)
 		{
-			return _cacheManager.Get(ALL_MAPPINGS_KEY, () =>
+			return $"attr.option.{languageId}.{optionId}";
+		}
+
+		protected virtual IDictionary<string, int> GetCachedIdByAliasMappings()
+		{
+			return _cacheManager.Get(ALL_ID_BY_ALIAS_MAPPINGS_KEY, () =>
 			{
 				var dictionary = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
 
@@ -87,9 +96,55 @@ namespace SmartStore.Services.Search.Modelling
 			});
 		}
 
+		protected virtual IDictionary<string, string> GetCachedAliasByIdMappings()
+		{
+			return _cacheManager.Get(ALL_ALIAS_BY_ID_MAPPINGS_KEY, () =>
+			{
+				var dictionary = new Dictionary<string, string>();
+
+				var attributes = _specificationAttributeService.GetSpecificationAttributes()
+					.Expand(x => x.SpecificationAttributeOptions)
+					.ToList();
+
+				var locAttributes = _localizedPropertyRepository.TableUntracked
+					.Where(x => x.LocaleKeyGroup == "SpecificationAttribute" && x.LocaleKey == "Alias" && x.LocaleValue != null && x.LocaleValue != string.Empty)
+					.ToList();
+
+				var locOptions = _localizedPropertyRepository.TableUntracked
+					.Where(x => x.LocaleKeyGroup == "SpecificationAttributeOption" && x.LocaleKey == "Alias" && x.LocaleValue != null && x.LocaleValue != string.Empty)
+					.ToList();
+
+				foreach (var attribute in attributes)
+				{
+					if (attribute.Alias.HasValue())
+					{
+						dictionary[CreateAttributeKey(0, attribute.Id)] = attribute.Alias;
+					}
+
+					foreach (var option in attribute.SpecificationAttributeOptions.Where(x => x.Alias.HasValue()))
+					{
+						dictionary[CreateOptionKey(0, option.Id)] = option.Alias;
+					}
+				}
+
+				foreach (var locAttribute in locAttributes)
+				{
+					dictionary[CreateAttributeKey(locAttribute.LanguageId, locAttribute.EntityId)] = locAttribute.LocaleValue;
+				}
+
+				foreach (var locOption in locOptions)
+				{
+					dictionary[CreateOptionKey(locOption.LanguageId, locOption.EntityId)] = locOption.LocaleValue;
+				}
+
+				return dictionary;
+			});
+		}
+
 		public void ClearCache()
 		{
-			_cacheManager.RemoveByPattern(ALL_MAPPINGS_KEY);
+			_cacheManager.RemoveByPattern(ALL_ID_BY_ALIAS_MAPPINGS_KEY);
+			_cacheManager.RemoveByPattern(ALL_ALIAS_BY_ID_MAPPINGS_KEY);
 		}
 
 		public int GetAttributeIdByAlias(string attributeAlias, int languageId = 0)
@@ -98,7 +153,7 @@ namespace SmartStore.Services.Search.Modelling
 
 			if (attributeAlias.HasValue())
 			{
-				var mappings = GetAllCachedMappings();
+				var mappings = GetCachedIdByAliasMappings();
 
 				if (!mappings.TryGetValue(CreateAttributeKey(languageId, attributeAlias), out result))
 				{
@@ -118,13 +173,53 @@ namespace SmartStore.Services.Search.Modelling
 
 			if (optionAlias.HasValue() && attributeId != 0)
 			{
-				var mappings = GetAllCachedMappings();
+				var mappings = GetCachedIdByAliasMappings();
 
 				if (!mappings.TryGetValue(CreateOptionKey(languageId, attributeId, optionAlias), out result))
 				{
 					if (languageId != 0)
 					{
 						mappings.TryGetValue(CreateOptionKey(0, attributeId, optionAlias), out result);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public string GetAttributeAliasById(int attributeId, int languageId = 0)
+		{
+			string result = null;
+
+			if (attributeId != 0)
+			{
+				var mappings = GetCachedAliasByIdMappings();
+
+				if (!mappings.TryGetValue(CreateAttributeKey(languageId, attributeId), out result))
+				{
+					if (languageId != 0)
+					{
+						mappings.TryGetValue(CreateAttributeKey(0, attributeId), out result);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public string GetOptionAliasById(int optionId, int languageId = 0)
+		{
+			string result = null;
+
+			if (optionId != 0)
+			{
+				var mappings = GetCachedAliasByIdMappings();
+
+				if (!mappings.TryGetValue(CreateOptionKey(languageId, optionId), out result))
+				{
+					if (languageId != 0)
+					{
+						mappings.TryGetValue(CreateOptionKey(0, optionId), out result);
 					}
 				}
 			}
--- src/Libraries/SmartStore.Services/Search/Modelling/ICatalogSearchQueryAliasMapper.cs
+++ src/Libraries/SmartStore.Services/Search/Modelling/ICatalogSearchQueryAliasMapper.cs
@@ -8,7 +8,7 @@
 		void ClearCache();
 
 		/// <summary>
-		/// Gets the attribute id for an attribute alias
+		/// Gets the attribute id by attribute alias
 		/// </summary>
 		/// <param name="attributeAlias">Attribute alias</param>
 		/// <param name="languageId">Language identifier</param>
@@ -16,12 +16,28 @@
 		int GetAttributeIdByAlias(string attributeAlias, int languageId = 0);
 
 		/// <summary>
-		/// Gets the attribute option id for an option alias
+		/// Gets the attribute option id by option alias
 		/// </summary>
 		/// <param name="optionAlias">Attribute option alias</param>
 		/// <param name="attributeId">Attribute identifier</param>
 		/// <param name="languageId">Language identifier</param>
 		/// <returns>Attribute option identifier</returns>
 		int GetOptionIdByAlias(string optionAlias, int attributeId, int languageId = 0);
+
+		/// <summary>
+		/// Gets the attribute alias by attribute id
+		/// </summary>
+		/// <param name="attributeId">Attribute identifier</param>
+		/// <param name="languageId">Language identifier</param>
+		/// <returns>Attribute alias</returns>
+		string GetAttributeAliasById(int attributeId, int languageId = 0);
+
+		/// <summary>
+		/// Get the attribute option alias by option id
+		/// </summary>
+		/// <param name="optionId">Option identifier</param>
+		/// <param name="languageId">Language identifier</param>
+		/// <returns>Attribute option alias</returns>
+		string GetOptionAliasById(int optionId, int languageId = 0);
 	}
 }

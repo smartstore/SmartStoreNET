d3de94626c2cc68c61cb5674af6e3295c12678ac
Michael Herzog <herzog@smartstore.com>
2017-02-09 18:47:23 +0100

    Minor correction

--- src/Presentation/SmartStore.Web/Administration/Validators/Catalog/ProductValidator.cs
+++ src/Presentation/SmartStore.Web/Administration/Validators/Catalog/ProductValidator.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
 using SmartStore.Admin.Models.Catalog;
 using SmartStore.Services.Localization;
+using System;
+using System.Linq;
 
 namespace SmartStore.Admin.Validators.Catalog
 {
@@ -9,11 +11,14 @@ namespace SmartStore.Admin.Validators.Catalog
         public ProductValidator(ILocalizationService localizationService)
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Products.Fields.Name.Required"));
-            RuleFor(x => x.OrderMinimumQuantity).GreaterThan(0).WithMessage(localizationService.GetResource("Admin.Validation.ValueGreaterZero"));
-            RuleFor(x => x.OrderMaximumQuantity).GreaterThan(0).WithMessage(localizationService.GetResource("Admin.Validation.ValueGreaterZero"));
 
+            When(x => x.LoadedTabs.Contains("Inventory", StringComparer.OrdinalIgnoreCase), () => {
+                RuleFor(x => x.OrderMinimumQuantity).GreaterThan(0).WithMessage(localizationService.GetResource("Admin.Validation.ValueGreaterZero"));
+                RuleFor(x => x.OrderMaximumQuantity).GreaterThan(0).WithMessage(localizationService.GetResource("Admin.Validation.ValueGreaterZero"));
+            });
+            
             // validate PAnGV
-            When(x => x.BasePriceEnabled, () =>
+            When(x => x.BasePriceEnabled && x.LoadedTabs.Contains("Price"), () =>
 			{
 				RuleFor(x => x.BasePriceMeasureUnit).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Products.Fields.BasePriceMeasureUnit.Required"));
 				RuleFor(x => x.BasePriceBaseAmount)

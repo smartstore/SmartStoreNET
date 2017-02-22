b0f3f3078ba88720f2315b1eb5d6584e1290f3f3
Michael Herzog <herzog@smartstore.com>
2017-02-22 19:15:50 +0100

    First steps for OffCanvasMenu

---
+++ src/Presentation/SmartStore.Web/Models/Catalog/AjaxCategoryModel.cs
@@ -0,0 +1,33 @@
+﻿using SmartStore.Web.Framework.Modelling;
+using System.Collections.Generic;
+
+namespace SmartStore.Web.Models.Catalog
+{
+    public partial class AjaxCategoryModel : EntityModelBase
+    {
+        public AjaxCategoryModel()
+        {
+            SubCategories = new List<AjaxCategoryModel>();
+            ParentCategory = new AjaxParentCategoryModel();
+        }
+
+        public string Name { get; set; }
+
+        public string SortDescription { get; set; }
+
+        public string SeName { get; set; }
+
+        public bool HasChildren { get; set; }
+
+        public IList<AjaxCategoryModel> SubCategories { get; set; }
+
+        public AjaxParentCategoryModel ParentCategory { get; set; }
+    }
+
+    public partial class AjaxParentCategoryModel : EntityModelBase
+    {
+        public string Name { get; set; }
+
+        public string SeName { get; set; }
+    }
+}
---
+++ src/Presentation/SmartStore.Web/Themes/Alpha/Scripts/public.offcanvas-menu.js
@@ -0,0 +1,111 @@
+﻿;
+
+/*
+** OffCanvasMenu
+*/
+
+var AjaxMenu = (function ($, window, document, undefined) {
+
+    var isInitialised = false;
+    var viewport = ResponsiveBootstrapToolkit;
+    var selectedMenuItemId = 0;
+
+    $(function () {
+
+        // if viewport <lg 
+        if (viewport.is('<lg')) {
+            AjaxMenu.initMenu();
+        }
+        
+        // listen to viewport changed event and init if appropriate
+        $(window).resize(
+            viewport.changed(function () {
+                if (viewport.is('<lg') && !isInitialised) {
+                    AjaxMenu.initMenu();
+                }
+            })
+        );
+
+        // listen to clicks inside of #offcanvas-menu
+        $("#offcanvas-menu").on('click', '.nav-item', function (e) {
+            
+            var item = $(this);
+            var entityId = item.data("entity-id");
+            var isAjaxNavigation = item.data("is-ajax-navigation");
+
+            if (isAjaxNavigation == false) {
+                // let event bubble up
+                return true;
+            }
+            
+            e.preventDefault();
+
+            navigateToMenuItem(entityId);
+
+            // for stopping event propagation
+            return false;
+        });
+	});
+
+	function navigateToMenuItem(entityId) {
+	    $.ajax({
+	        cache: false,
+	        //url: '@(Url.Action("ChildCategories", "Catalog"))',
+	        url: 'Catalog/ChildCategories',
+	        data: { "categoryId": entityId },
+	        type: 'POST',
+	        success: function (response) {
+	            // replace current cat container content with response
+	            $("#offcanvas-menu-catalog").html(response);
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	            console.log(errorThrown);
+	        },
+	        complete: function () { }
+	    });
+	}
+
+	return {
+
+	    initMenu: function () {
+
+	        var offcanvasMenu = $('<aside id="offcanvas-menu" class="offcanvas x-offcanvas-lg offcanvas-overlay offcanvas-shadow" data-overlay="true" data-noscroll="true" data-blocker="true"><div class="offcanvas-content"></div></aside>')
+                .appendTo('body');
+
+	        var menuContent = $(".menubar-section .menubar");
+	        menuContent.clone().appendTo(offcanvasMenu.children().first());
+
+            // cosmetics: remove classes and restructure given HTML of header menu
+	        offcanvasMenu.find(".menubar-group").removeClass("pull-left");
+
+	        /*
+            var categories = $(".megamenu .navbar-nav");
+	        categories
+                .clone()
+                .appendTo(offcanvasMenu.children().first());
+
+	        $(".navbar-nav", offcanvasMenu).wrap("<div id='offcanvas-menu-catalog'></div>")
+            */
+
+	        offcanvasMenu.children().first().append("<div id='offcanvas-menu-catalog'><ul class='navbar-nav'></ul></div>");
+
+	        $("#shopbar-menu").click(function () {
+	            offcanvasMenu.offcanvas('show');
+	        });
+
+	        //selectedMenuItemId = categories.data("selected-menu-item");
+	        selectedMenuItemId = $(".megamenu .navbar-nav").data("selected-menu-item");
+	        navigateToMenuItem(selectedMenuItemId);
+
+            /*
+	        if (selectedMenuItemId != 0)
+	        {
+	            navigateToMenuItem(selectedMenuItemId);
+	        }
+            */
+
+	        isInitialised = true;
+		}
+	}
+
+})(jQuery, this, document);

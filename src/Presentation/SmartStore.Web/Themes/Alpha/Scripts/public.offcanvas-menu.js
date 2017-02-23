ef4221420b7abfff9772edab28ba784a25c2fbfd
Michael Herzog <herzog@smartstore.com>
2017-02-23 19:13:32 +0100

    Some restructuring and animation for OffCanvasMenu

--- src/Presentation/SmartStore.Web/Themes/Alpha/Scripts/public.offcanvas-menu.js
+++ src/Presentation/SmartStore.Web/Themes/Alpha/Scripts/public.offcanvas-menu.js
@@ -34,20 +34,20 @@ var AjaxMenu = (function ($, window, document, undefined) {
             var isAjaxNavigation = item.data("is-ajax-navigation");
 
             if (isAjaxNavigation == false) {
-                // let event bubble up
+                // let event bubble up, so normal navigation via href attribute takes effect
                 return true;
             }
             
             e.preventDefault();
 
-            navigateToMenuItem(entityId);
+            navigateToMenuItem(entityId, item.hasClass("back-to-parent-cat") ? "right" : "left");
 
             // for stopping event propagation
             return false;
         });
 	});
 
-	function navigateToMenuItem(entityId) {
+	function navigateToMenuItem(entityId, direction) {
 	    $.ajax({
 	        cache: false,
 	        //url: '@(Url.Action("ChildCategories", "Catalog"))',
@@ -55,8 +55,47 @@ var AjaxMenu = (function ($, window, document, undefined) {
 	        data: { "categoryId": entityId },
 	        type: 'POST',
 	        success: function (response) {
+
 	            // replace current cat container content with response
-	            $("#offcanvas-menu-catalog").html(response);
+
+	            var categoryContainer = $(".category-container:first");
+	            var firstCall = categoryContainer.length == 0;
+	            
+	            if (firstCall) {
+	                $("#offcanvas-menu-catalog").append(response);
+	            }
+	            else {
+
+	                var responseHtml = response.replace("category-container", "category-container category-container-slide-in category-container-slide-in--from-" + direction);
+	                
+	                $("#offcanvas-menu-catalog").append(responseHtml);
+	                
+	                var menuWidth = $("#offcanvas-menu-catalog").width();
+	                var categoryContainerSlideIn = $(".category-container-slide-in");
+
+	                categoryContainerSlideIn.css("width", menuWidth + "px");
+
+	                if (direction == "left") {
+	                    categoryContainerSlideIn.css("left", menuWidth + "px");
+	                    categoryContainer.css("margin-left", "-" + menuWidth + "px");
+	                    categoryContainerSlideIn.css("left", "0px");
+	                }
+	                else {
+	                    //categoryContainerSlideIn.css("right", "-" + menuWidth + "px");
+	                    categoryContainer.css("margin-left", menuWidth + "px");
+	                    categoryContainerSlideIn.css("right", "0px");
+	                }
+
+	                // remove slid container after transition
+	                _.delay(function () {
+
+	                    categoryContainer.remove();
+
+	                    // finaly remove temporary class
+	                    categoryContainerSlideIn.removeClass("category-container-slide-in");
+
+	                }, 600);
+	            }
 	        },
 	        error: function (jqXHR, textStatus, errorThrown) {
 	        	console.log(errorThrown);
@@ -79,6 +118,11 @@ var AjaxMenu = (function ($, window, document, undefined) {
             // cosmetics: remove classes and restructure given HTML of header menu
 	        offcanvasMenu.find(".menubar-group").removeClass("pull-left");
 
+	        var ownMenu = '<div class="button-bar">';
+	        ownMenu += '<a class="back-to-home m-r-05" href="@Url.RouteUrl("HomePage")"><i class="fa fa-home"></i>Home</a>';
+	        ownMenu += '<a class="close-offcanvas-menu offcanvas-closer"><i class="fa fa-times"></i>Schließen</a>';
+	        ownMenu += '</div>';
+
 	        /*
             var categories = $(".megamenu .navbar-nav");
 	        categories
@@ -97,13 +141,6 @@ var AjaxMenu = (function ($, window, document, undefined) {
 	        //selectedMenuItemId = categories.data("selected-menu-item");
 	        selectedMenuItemId = $(".megamenu .navbar-nav").data("selected-menu-item");
 	        navigateToMenuItem(selectedMenuItemId);
-
-            /*
-	        if (selectedMenuItemId != 0)
-	        {
-	            navigateToMenuItem(selectedMenuItemId);
-	        }
-            */
 
 	        isInitialised = true;
 		}

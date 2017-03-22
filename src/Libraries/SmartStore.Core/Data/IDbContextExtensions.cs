ca408d1b545a8f3d16ef39979d94b0104267bf4b
Murat Cakir <muratc@smartstore.de>
2017-03-22 21:45:41 +0100

    Can pass queryAction to IDbContext.LoadReference() now

--- src/Libraries/SmartStore.Core/Data/IDbContextExtensions.cs
+++ src/Libraries/SmartStore.Core/Data/IDbContextExtensions.cs
@@ -140,7 +140,8 @@ namespace SmartStore
 			this IDbContext ctx,
 			TEntity entity,
 			Expression<Func<TEntity, TProperty>> navigationProperty,
-			bool force = false)
+			bool force = false,
+			Func<IQueryable<TProperty>, IQueryable<TProperty>> queryAction = null)
 			where TEntity : BaseEntity
 			where TProperty : BaseEntity
 		{
@@ -163,7 +164,23 @@ namespace SmartStore
 
 			if (!reference.IsLoaded)
 			{
-				reference.Load();
+				if (queryAction != null || ctx.ForceNoTracking)
+				{
+					var query = !ctx.ForceNoTracking
+						? reference.Query()
+						: reference.Query().AsNoTracking();
+
+					var myQuery = queryAction != null
+						? queryAction(query)
+						: query;
+
+					reference.CurrentValue = myQuery.FirstOrDefault();
+				}
+				else
+				{
+					reference.Load();
+				}		
+
 				reference.IsLoaded = true;
 			}
 		}

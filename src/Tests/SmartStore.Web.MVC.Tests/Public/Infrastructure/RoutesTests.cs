cbf314ccf073c43631ac3d502128c8216625680e
Marcus  Gesing <home@footwear-box.de>
2017-05-10 10:54:01 +0200

    Fixes compilation error

--- src/Tests/SmartStore.Web.MVC.Tests/Public/Infrastructure/RoutesTests.cs
+++ src/Tests/SmartStore.Web.MVC.Tests/Public/Infrastructure/RoutesTests.cs
@@ -161,7 +161,6 @@ namespace SmartStore.Web.MVC.Tests.Public.Infrastructure
         {
             "~/contactus".ShouldMapTo<HomeController>(c => c.ContactUs());
 			"~/sitemap.xml".ShouldMapTo<HomeController>(c => c.SitemapSeo(1));
-            "~/settings".ShouldMapTo<CommonController>(c => c.Settings());
         }
 
         [Test]

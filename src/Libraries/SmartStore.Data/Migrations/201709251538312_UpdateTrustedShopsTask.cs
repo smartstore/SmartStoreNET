c5e3fcab6ee1b1ca554824702a57ae6728d1c4ee
Michael Herzog <herzog@smartstore.com>
2017-09-25 18:54:46 +0200

    Added migration to update TrustedShops tasks

---
+++ src/Libraries/SmartStore.Data/Migrations/201709251538312_UpdateTrustedShopsTask.cs
@@ -0,0 +1,50 @@
+namespace SmartStore.Data.Migrations
+{
+    using Core.Domain.Tasks;
+    using Setup;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public partial class UpdateTrustedShopsTask : DbMigration, IDataSeeder<SmartObjectContext>
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+
+        public bool RollbackOnFailure
+        {
+            get { return false; }
+        }
+
+        public void Seed(SmartObjectContext context)
+        {
+            var table = context.Set<ScheduleTask>();
+            var taskToDelete = table.Where(x => x.Type.Contains("SmartStore.TrustedShops.UpdateRatingWidgetStateTask")).FirstOrDefault();
+
+            if (taskToDelete != null)
+            {
+                // remove old task
+                table.Remove(taskToDelete);
+                
+                // if task to remnove is not null we assume the plugin is currently installed and so we add the new task without any further request
+                var taskToAdd = new ScheduleTask
+                {
+                    Name = "Trusted Shops Review Import",
+                    CronExpression = "0 4 * * *", // At 04:00 am
+                    Type = "SmartStore.TrustedShops.ImportProductReviewsTask, SmartStore.TrustedShops",
+                    Enabled = false,
+                    StopOnError = true
+                };
+
+                table.Add(taskToAdd);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}

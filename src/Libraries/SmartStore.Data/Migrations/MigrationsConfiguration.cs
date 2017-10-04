43e1785d8d963b998c6ee8eaf86d1673d239b549
Michael Herzog <herzog@smartstore.com>
2017-10-04 19:00:09 +0200

    Restructured catalog settings

--- src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
+++ src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
@@ -41,10 +41,15 @@ namespace SmartStore.Data.Migrations
             builder.AddOrUpdate("Admin.Configuration.Settings.Catalog.MaxItemsToDisplayInCatalogMenu",
                 "Max items to display in catalog menu",
                 "Maximale Anzahl von Elementen in der Katalognavigation",
-                "Defines the maximum number of top level items to display in main catalog menu.",
-                "Legt die maximale Anzahl von Menu-Einträgen der obersten Hierarchie fest, die in der Katalognavigation angezeigt werden.");
+                "Defines the maximum number of top level items to be displayed in the main catalog menu. All menu items which are exceeding this limit will be placed in a new dropdown menu item.",
+                "Legt die maximale Anzahl von Menu-Einträgen der obersten Hierarchie fest, die in der Katalognavigation angezeigt werden. Alle weiteren Menu-Einträge werden innerhalb eines neuen Dropdownmenus ausgegeben.");
 
             builder.AddOrUpdate("CatalogMenu.MoreLink", "More", "Mehr");
+
+            builder.AddOrUpdate("Admin.CatalogSettings.Homepage", "Homepage", "Homepage");
+            builder.AddOrUpdate("Admin.CatalogSettings.ProductDisplay", "Product display", "Produktdarstellung");
+            builder.AddOrUpdate("Admin.CatalogSettings.Prices", "Prices", "Preise");
+            builder.AddOrUpdate("Admin.CatalogSettings.CompareProducts", "Compare products", "Produktvergleich");
         }
 	}
 }

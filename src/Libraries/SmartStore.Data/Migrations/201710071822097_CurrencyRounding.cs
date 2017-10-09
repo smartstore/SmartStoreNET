d94084f574c4592cc710f2cc467babff791f643f
Murat Cakir <muratc@smartstore.de>
2017-10-10 01:42:50 +0200

    Changed some language resources

--- src/Libraries/SmartStore.Data/Migrations/201710071822097_CurrencyRounding.cs
+++ src/Libraries/SmartStore.Data/Migrations/201710071822097_CurrencyRounding.cs
@@ -43,64 +43,64 @@ namespace SmartStore.Data.Migrations
             builder.AddOrUpdate("ShoppingCart.Totals.Rounding", "Rounding", "Rundung");
 
             builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Directory.CurrencyRoundingRule.RoundMidpointDown",
-                "Round midpoint down (e.g. 0.05 rounding, 9.225 will round to 9.20)",
-                "Mittelwert abrunden (z.B. 0,05 Rundung, 9,225 wird auf 9,20 gerundet)");
+                "Round midpoint down (e.g. 0.05 rounding: 9.225 will round to 9.20)",
+                "Mittelwert abrunden (z.B. 0,05 Rundung: 9,225 wird auf 9,20 gerundet)");
             builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Directory.CurrencyRoundingRule.RoundMidpointUp",
-                "Round midpoint up (e.g. 0.05 rounding, 9.225 will round to 9.25)",
-                "Mittelwert aufrunden (z.B. 0,05 Rundung, 9,225 wird auf 9,25 gerundet)");
+                "Round midpoint up (e.g. 0.05 rounding: 9.225 will round to 9.25)",
+                "Mittelwert aufrunden (z.B. 0,05 Rundung: 9,225 wird auf 9,25 gerundet)");
             builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Directory.CurrencyRoundingRule.AlwaysRoundDown",
-                "Always round down (e.g. 0.05 rounding, 9.24 will round to 9.20)",
-                "Immer abrunden (z.B. 0,05 Rundung, 9,24 wird auf 9,20 gerundet)");
+                "Always round down (e.g. 0.05 rounding: 9.24 will round to 9.20)",
+                "Immer abrunden (z.B. 0,05 Rundung: 9,24 wird auf 9,20 gerundet)");
             builder.AddOrUpdate("Enums.SmartStore.Core.Domain.Directory.CurrencyRoundingRule.AlwaysRoundUp",
                 "Always round up (e.g. 0.05 rounding, 9.26 will round to 9.30)",
-                "Immer abrunden (z.B. 0,05 Rundung, 9,26 wird auf 9,30 gerundet)");
+                "Immer aufrunden (z.B. 0,05 Rundung: 9,26 wird auf 9,30 gerundet)");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.NoPaymentMethodsEnabledRounding",
-                "The rounding of the order value is not activated for any payment methid. Click <a href='{0}'>edit</a> for the desired payment methods and activate the rounding of the order total.",
-                "Die Rundung des Auftragswertes ist bei keiner Zahlart aktiviert. Klicken Sie bei den gewünschten Zahlarten auf <a href='{0}'>Bearbeiten</a> und aktivieren Sie die Rundung des Auftragswertes.");
+				"Regardless of the currency configuration, the order totals are only rounded if it has been enabled for the selected payment method. Click <a href='{0}'>Edit</a> for the desired payment methods and activate the rounding option.",
+				"Unabhängig von der Währungs-Konfiguration werden Bestellsummen erst gerundet, wenn das für die gewählte Zahlart auch vorgesehen ist. Klicken Sie bei den gewünschten Zahlarten auf <a href='{0}'>Bearbeiten</a> und aktivieren Sie die Rundungs-Option.");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.PaymentMethodsEnabledRoundingList",
-                "Payment methods for which rounding of the order total is enabled",
-                "Zahlarten, bei denen die Rundung des Auftragswertes aktiviert ist");
+				"Payment methods for which order total rounding is enabled",
+                "Zahlarten, bei denen Bestellsummen-Rundung aktiviert ist");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundDuringCalculation",
-                "Round during price calculation",
-                "Während der Preisberechnung runden",
-                "Specifies whether to round during price calculation.",
-                "Legt fest, ob bereits während der Preisberechnung gerundet werden soll.");
+                "Round all order item amounts",
+                "Beträge aller Bestellpositionen runden",
+                "Specifies whether to round all order item amounts (products, fees, tax etc.)",
+                "Legt fest, ob die Beträge aller Bestellpositionen gerundet werden sollen (Produkte, Gebühren, Steuern etc.)");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundDuringCalculationDecimals",
-                "Number of decimal places",
-                "Anzahl der Dezimalstellen",
-                "Specifies the number of decimal places to round to. 2 is the default value.",
-                "Legt fest, auf wieviele Dezimalstellen gerundet werden soll. 2 ist der Standardwert.");
+                "Number of decimal digits",
+                "Anzahl Dezimalstellen",
+                "Specifies the number of decimal digits to round to (Default: 2)",
+                "Legt fest, auf wieviele Dezimalstellen gerundet werden soll (Standard: 2)");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundOrderTotal",
-                "Round order total",
-                "Auftragswert runden",
-                "Specifies whether to round the order total.",
-                "Legt fest, ob der Gesamtwert eines Auftrages gerundet werden soll.");
+                "Round order total amount",
+                "Bestellsumme runden",
+                "Specifies whether to round the order total amount.",
+                "Legt fest, ob die Bestellsumme gerundet werden soll.");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundOrderTotalToValue",
                 "Round to",
                 "Runden nach",
-                "Specifies the nearest multiple of the smallest, chosen amount to round the order total to. For 0.05 for example 9.43 will be rounded to 9.45.",
-                "Legt das nächste Vielfache des kleinsten, gewählten Betrages fest, auf den der Aufragswert gerundet werden soll. Bei 0,05 wird z.B. 9.43 auf 9.45 gerundet.");
+                "Specifies the nearest multiple of the smallest chosen amount to round the order total to. 0.05 for example will round 9.43 up to 9.45.",
+                "Legt das nächste Vielfache des kleinsten, gewählten Betrages fest, auf den die Bestellsumme gerundet werden soll. Bei 0,05 wird z.B. 9,43 auf 9,45 gerundet.");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundOrderTotalRule",
                 "Rounding rule",
                 "Rundungsregel",
-                "Specifies the rule for rounding the order total.",
-                "Legt die Rundungsregel für das Runden des Auftragswertes fest.");
+                "Specifies the rule for rounding the order total amount.",
+                "Legt die Regel für das Runden der Bestellsumme fest.");
 
             builder.AddOrUpdate("Admin.Configuration.Payment.Methods.RoundOrderTotal",
-                "Apply rounding, if enabled",
-                "Runden anwenden, sofern aktiviert",
-                "Specifies whether to round the order total for this payment method if rounding for the chosen currency is enabled.",
-                "Legt fest, ob die Rundung des Auftragswertes für diese Zahlart angewendet werden soll, vorausgesetzt Runden ist für die gewählte Währung aktiviert.");
+                "Round order total amount (if enabled)",
+                "Bestellsumme runden, sofern aktiviert",
+                "Specifies whether to round the order total in accordance with currency configuration if this payment method was selected in checkout.",
+                "Legt fest, ob die Bestellsumme gemäß Währungs-Konfiguration gerundet werden soll, wenn diese Zahlart im Checkout gewählt wurde.");
 
             builder.AddOrUpdate("Admin.Configuration.Currencies.Fields.RoundDuringCalculationDecimals.Validation",
-                "The number of decimal places must be between 0 and 8.",
+                "The number of decimal digits must be between 0 and 8.",
                 "Die Anzahl der Dezimalstellen muss zwischen 0 und 8 liegen.");
 
             builder.Delete(

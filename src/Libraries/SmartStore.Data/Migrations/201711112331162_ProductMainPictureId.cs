8016fa21f3583cd5dc80bd67648cbce5041aaeb1
Murat Cakir <muratc@smartstore.de>
2017-11-12 01:49:04 +0100

    (Perf) added MainPictureId property to Product entity

---
+++ src/Libraries/SmartStore.Data/Migrations/201711112331162_ProductMainPictureId.cs
@@ -0,0 +1,84 @@
+namespace SmartStore.Data.Migrations
+{
+	using System;
+	using System.Linq;
+	using System.Data.Entity;
+	using System.Collections.Generic;
+	using System.Data.Entity.Migrations;
+	using Core.Domain.Catalog;
+	using Setup;
+	using Core;
+
+	public partial class ProductMainPictureId : DbMigration, IDataSeeder<SmartObjectContext>
+	{
+		public override void Up()
+        {
+            AddColumn("dbo.Product", "MainPictureId", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Product", "MainPictureId");
+        }
+
+		public bool RollbackOnFailure
+		{
+			get { return true; }
+		}
+
+		public void Seed(SmartObjectContext context)
+		{
+			var query = context.Set<Product>()
+				.AsNoTracking()
+				.Where(x => x.MainPictureId == null)
+				.OrderBy(x => x.Id)
+				.Select(x => x.Id);
+
+			int pageIndex = 0;
+			var productIds = new PagedList<int>(query, pageIndex, 1000);
+
+			do
+			{
+				var map = GetPoductPictureMap(context, productIds);
+
+				using (var tx = context.Database.BeginTransaction())
+				{
+					foreach (var kvp in map)
+					{
+						context.ExecuteSqlCommand("Update [Product] Set [MainPictureId] = {0} WHERE [Id] = {1}", false, null, kvp.Value, kvp.Key);
+					}
+
+					if (map.Any())
+					{
+						context.SaveChanges();
+						tx.Commit();
+					}
+				}
+
+
+				productIds = new PagedList<int>(query, pageIndex++, 1000);
+			}
+			while (productIds.HasNextPage);
+		}
+
+		private IDictionary<int, int> GetPoductPictureMap(SmartObjectContext context, IEnumerable<int> productIds)
+		{
+			var map = new Dictionary<int, int>();
+
+			var query = from pp in context.Set<ProductPicture>().AsNoTracking()
+						where productIds.Contains(pp.ProductId)
+						group pp by pp.ProductId into g
+						select new
+						{
+							ProductId = g.Key,
+							PictureIds = g.OrderBy(x => x.DisplayOrder)
+								.Take(1)
+								.Select(x => x.PictureId)
+						};
+
+			map = query.ToList().ToDictionary(x => x.ProductId, x => x.PictureIds.First());
+
+			return map;
+		}
+	}
+}

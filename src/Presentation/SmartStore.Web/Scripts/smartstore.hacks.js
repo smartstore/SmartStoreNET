1b2cfeec6e962625816a905e932f08f6ab743a93
Murat Cakir <muratc@smartstore.de>
2018-01-26 02:00:03 +0100

    ABS4: Grid toolbar

--- src/Presentation/SmartStore.Web/Scripts/smartstore.hacks.js
+++ src/Presentation/SmartStore.Web/Scripts/smartstore.hacks.js
@@ -1,25 +1,16 @@
-﻿
-var Hacks = {
-
-    Telerik: {
-        // TODO: temp > Handle Button with MVC
+﻿var Hacks = {
+	Telerik: {
+		// TODO: temp > Handle Button with MVC
 		handleButton: function (el) {
-            el.removeClass("t-button").addClass("btn");
-            el.each(function () {
+			el.each(function () {
 				var btn = $(this);
-
-                if (btn.hasClass("t-grid-add")) {
-                    btn.addClass("btn-warning").prepend('<i class="fa fa-plus mr-2"></i>');
-                }
-                else if (btn.hasClass("t-grid-save-changes")) {
-                	btn.addClass("btn-primary").prepend('<i class="fa fa-check mr-2"></i>');
-                }
-                else if (btn.hasClass("t-grid-cancel-changes")) {
-                	btn.addClass("btn-warning");
-                }
-            });
-
-        }
-    }
-
-}
+				if (btn.hasClass("t-grid-add") && btn.find('> .fa').length === 0) {
+					btn.prepend('<i class="fa fa-plus mr-2"></i>');
+				}
+				else if (btn.hasClass("t-grid-save-changes") && btn.find('> .fa').length === 0) {
+					btn.prepend('<i class="fa fa-check mr-2"></i>');
+				}
+			});
+		}
+	}
+};

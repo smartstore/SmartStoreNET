b4a4fc209b8d14ef268ca61272cc014f11f2447b
Marcus  Gesing <home@footwear-box.de>
2018-02-01 14:09:41 +0100

    x-editable DateTime: removed class datetimefield (is obsolete too)

--- src/Presentation/SmartStore.Web/Content/x-editable/bootstrap-editable.js
+++ src/Presentation/SmartStore.Web/Content/x-editable/bootstrap-editable.js
@@ -843,11 +843,6 @@ Editableform is linked with one of input types, e.g. 'text', 'select' etc.
                } 
            }
            
-           //`datetime` should be datetimefield in 'inline' mode
-           if(type === 'datetime' && options.mode === 'inline') {
-             type = 'datetimefield';  
-           }           
-
            //change wysihtml5 to textarea for jquery UI and plain versions
            if(type === 'wysihtml5' && !$.fn.editabletypes[type]) {
                type = 'textarea';
@@ -6359,82 +6354,5 @@ $(function(){
     });
 
     $.fn.editabletypes.datetime = DateTime;
-
-}(window.jQuery));
-/**
-Bootstrap datetimefield input - datetime input for inline mode.
-Shows normal <input type="text"> and binds popup datetimepicker.  
-Automatically shown in inline mode.
-
-@class datetimefield
-@extends datetime
-
-**/
-(function ($) {
-    "use strict";
-    
-    var DateTimeField = function (options) {
-        this.init('datetimefield', options, DateTimeField.defaults);
-        this.initPicker(options, DateTimeField.defaults);
-    };
-
-    $.fn.editableutils.inherit(DateTimeField, $.fn.editabletypes.datetime);
-    
-    $.extend(DateTimeField.prototype, {
-        render: function () {
-            this.$input = this.$tpl.find('input');
-            this.setClass();
-            this.setAttr('placeholder');
-            
-            this.$tpl.datetimepicker(this.options.datetimepicker);
-            
-            //need to disable original event handlers
-            this.$input.off('focus keydown');
-            
-            //update value of datepicker
-            this.$input.keyup($.proxy(function(){
-               this.$tpl.removeData('date');
-               this.$tpl.datetimepicker('update');
-            }, this));
-            
-        },   
-      
-       value2input: function(value) {
-           this.$input.val(this.value2html(value));
-           this.$tpl.datetimepicker('update');
-       },
-        
-       input2value: function() { 
-           return this.html2value(this.$input.val());
-       },              
-        
-       activate: function() {
-           $.fn.editabletypes.text.prototype.activate.call(this);
-       },
-       
-       autosubmit: function() {
-         //reset autosubmit to empty  
-       }
-    });
-    
-    DateTimeField.defaults = $.extend({}, $.fn.editabletypes.datetime.defaults, {
-        /**
-        @property tpl 
-        **/         
-        tpl:'<div class="input-append date"><input type="text"/><span class="add-on"><i class="icon-th"></i></span></div>',
-        /**
-        @property inputclass 
-        @default 'input-medium'
-        **/         
-        inputclass: 'input-medium',
-        
-        /* datetimepicker config */
-        datetimepicker:{
-            todayHighlight: false,
-            autoclose: true
-        }
-    });
-    
-    $.fn.editabletypes.datetimefield = DateTimeField;
 
 }(window.jQuery));

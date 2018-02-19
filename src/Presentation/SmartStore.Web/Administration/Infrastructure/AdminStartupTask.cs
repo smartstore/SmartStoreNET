6cedec774f665f98ac166d049b6e13b41fcaab4a
Murat Cakir <muratc@smartstore.de>
2018-02-19 21:35:41 +0100

    ABS4: reorganized telerik component's asset file structure

--- src/Presentation/SmartStore.Web/Administration/Infrastructure/AdminStartupTask.cs
+++ src/Presentation/SmartStore.Web/Administration/Infrastructure/AdminStartupTask.cs
@@ -2,6 +2,7 @@
 using SmartStore.Core.Infrastructure;
 using SmartStore.Services.DataExchange.Csv;
 using SmartStore.Services.DataExchange.Import;
+using Telerik.Web.Mvc;
 
 namespace SmartStore.Admin.Infrastructure
 {
@@ -11,6 +12,9 @@ namespace SmartStore.Admin.Infrastructure
         {
 			TypeConverterFactory.RegisterConverter<CsvConfiguration>(new CsvConfigurationConverter());
 			TypeConverterFactory.RegisterConverter<ColumnMapConverter>(new ColumnMapConverter());
+
+			WebAssetDefaultSettings.ScriptFilesPath = "~/Administration/Content/telerik/js";
+			WebAssetDefaultSettings.StyleSheetFilesPath = "~/Administration/Content/telerik/css";
 		}
 
         public int Order

75cdd1d4c2836f966773bf3ed998a66e86d78ebc
Murat Cakir <muratc@smartstore.de>
2018-02-24 03:21:32 +0100

    Completely removed CKEditor from solution

--- src/Presentation/SmartStore.Web/Content/editors/ckeditor/skins/smartstore/skin.js
+++
@@ -1,9 +0,0 @@
-﻿/*
- Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-CKEDITOR.skin.name = "smartstore";
-CKEDITOR.skin.ua_editor = "";
-CKEDITOR.skin.ua_dialog = "";
-CKEDITOR.skin.ua = { };
-CKEDITOR.skin.chameleon = null;

456887d8219120df978b1e9445cb62db1b5385c7
Murat Cakir <muratc@smartstore.de>
2018-10-12 03:35:54 +0200

    New EditorTemplate "Range"

--- src/Libraries/SmartStore.Core/Extensions/TypeExtensions.cs
+++ src/Libraries/SmartStore.Core/Extensions/TypeExtensions.cs
@@ -26,7 +26,34 @@ namespace SmartStore
 	        return null;
         }
 
-        public static bool IsSequenceType(this Type type)
+		public static bool IsNumericType(this Type type)
+		{
+			switch (Type.GetTypeCode(type))
+			{
+				case TypeCode.Byte:
+				case TypeCode.SByte:
+				case TypeCode.UInt16:
+				case TypeCode.UInt32:
+				case TypeCode.UInt64:
+				case TypeCode.Int16:
+				case TypeCode.Int32:
+				case TypeCode.Int64:
+				case TypeCode.Decimal:
+				case TypeCode.Double:
+				case TypeCode.Single:
+					return true;
+				case TypeCode.Object:
+					if (type.IsNullable(out var innerType))
+					{
+						return innerType.IsNumericType();
+					}
+					return false;
+				default:
+					return false;
+			}
+		}
+
+		public static bool IsSequenceType(this Type type)
         {
 			if (type == typeof(string))
 				return false;

ecb58eb936c661041fcc19b6c13e6df8e3058617
Marcus  Gesing <home@footwear-box.de>
2019-01-24 17:00:03 +0100

    More on StoryExportAssetAttribute

--- src/Presentation/SmartStore.Web.Framework/Modelling/StoryExportAssetAttribute.cs
+++ src/Presentation/SmartStore.Web.Framework/Modelling/StoryExportAssetAttribute.cs
@@ -8,15 +8,34 @@ namespace SmartStore.Web.Framework.Modelling
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class StoryExportAssetAttribute : Attribute
     {
-        public StoryExportAssetAttribute(StoryExportPropertyType type)
+        public StoryExportAssetAttribute(
+            StoryExportPropertyType type,
+            Type root,
+            string rootProperty)
         {
+            Guard.NotNull(root, nameof(root));
+            Guard.NotEmpty(rootProperty, nameof(rootProperty));
+
             Type = type;
+            Root = root;
+            RootProperty = rootProperty;
         }
 
         /// <summary>
-        /// The property type.
+        /// The asset property type.
         /// </summary>
         public StoryExportPropertyType Type { get; private set; }
+
+        /// <summary>
+        /// The root entity type that contains the model data.
+        /// </summary>
+        public Type Root { get; private set; }
+
+        /// <summary>
+        /// The root property name that contains the model data.
+        /// It is expected that the property value is always a Json serialized string.
+        /// </summary>
+        public string RootProperty { get; private set; }
     }
 
 
@@ -26,5 +45,13 @@ namespace SmartStore.Web.Framework.Modelling
         /// The property value is a picture identifier.
         /// </summary>
         PictureId = 0
+    }
+
+
+    /// <summary>
+    /// Required to identify export assets containing models.
+    /// </summary>
+    public interface IStoryExportModel
+    {
     }
 }

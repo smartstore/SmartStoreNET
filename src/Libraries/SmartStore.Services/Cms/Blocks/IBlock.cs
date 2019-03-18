d254e386df23bda8a60fe163047c53739d08f4a8
Murat Cakir <muratc@smartstore.de>
2019-03-19 00:52:04 +0100

    Code docs

--- src/Libraries/SmartStore.Services/Cms/Blocks/IBlock.cs
+++ src/Libraries/SmartStore.Services/Cms/Blocks/IBlock.cs
@@ -3,6 +3,10 @@ using System.Web.Routing;
 
 namespace SmartStore.Services.Cms.Blocks
 {
+	/// <summary>
+	/// Marker interface for a PageBuilder block implementation.
+	/// Blocks are handled by corresponding <see cref="IBlockHandler"/> implementations.
+	/// </summary>
 	public interface IBlock
 	{
 	}

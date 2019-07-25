1062ae5ca501d6ca915bb025eb7f6bb0096d655f
Marcus  Gesing <home@footwear-box.de>
2019-07-25 14:35:57 +0200

    Granular permissions: further work

--- src/Libraries/SmartStore.Services/Security/StandardPermissionProvider2.cs
+++ src/Libraries/SmartStore.Services/Security/StandardPermissionProvider2.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using SmartStore.Core.Domain.Customers;
 using SmartStore.Core.Domain.Security;
 
 namespace SmartStore.Services.Security
@@ -7,12 +11,79 @@ namespace SmartStore.Services.Security
     {
         public virtual IEnumerable<PermissionRecord> GetPermissions()
         {
-            return new PermissionRecord[] { };
+            var permissions = new List<PermissionRecord>();
+            GetPermissionsFor(typeof(PermissionSystemNames), permissions);
+
+            return permissions;
         }
 
         public virtual IEnumerable<DefaultPermissionRecord> GetDefaultPermissions()
         {
-            return new DefaultPermissionRecord[] { };
+            return new[]
+            {
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Administrators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = PermissionSystemNames.Catalog.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Customer.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Order.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Promotion.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cms.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Configuration.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.System.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.Self },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Media.Self }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.ForumModerators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = PermissionSystemNames.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = PermissionSystemNames.System.AccessShop }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Guests,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = PermissionSystemNames.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = PermissionSystemNames.System.AccessShop }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Registered,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = PermissionSystemNames.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = PermissionSystemNames.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = PermissionSystemNames.System.AccessShop }
+                    }
+                }
+            };
+        }
+
+        private void GetPermissionsFor(Type type, List<PermissionRecord> result)
+        {
+            var permissions = type
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(string))
+                .Select(x => new PermissionRecord { SystemName = (string)x.GetRawConstantValue() });
+
+            result.AddRange(permissions);
+
+            var nestedTypes = type.GetNestedTypes(BindingFlags.Public | BindingFlags.Static);
+            nestedTypes.Each(x => GetPermissionsFor(x, result));
         }
     }
 }

91b5205f9830397f65cee564629689611f71645c
Marcus  Gesing <home@footwear-box.de>
2019-08-20 12:34:11 +0200

    Make MeasureDimension and MeasureWeight localizable (in progress)

--- src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
+++ src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
@@ -50,94 +50,10 @@
 
 		public void MigrateLocaleResources(LocaleResourcesBuilder builder)
 		{
-            builder.AddOrUpdate("Checkout.OrderCompletes",
-                "Your order will be completed.",
-                "Ihre Bestellung wird abgeschlossen.");
+            builder.AddOrUpdate("Admin.Configuration.Measures.Weights.AddWeight", "Add weight", "Gewicht hinzufügen");
+            builder.AddOrUpdate("Admin.Configuration.Measures.Weights.EditWeight", "Edit weight", "Gewicht bearbeiten");
 
-            builder.AddOrUpdate("Admin.Catalog.Attributes.CheckoutAttributes.Fields.TextPrompt",
-                "Text prompt",
-                "Text Eingabeaufforderung",
-                "Specifies the prompt text.",
-                "Legt den Text zur Eingabeaufforderung fest.");
-
-            builder.AddOrUpdate("Admin.Catalog.Products.ProductVariantAttributes.Attributes.Fields.TextPrompt",
-                "Text prompt",
-                "Text Eingabeaufforderung");
-
-            builder.AddOrUpdate("Admin.Catalog.Categories.Fields.ExternalLink",
-                "External link",
-                "Externer Link",
-                "Alternative external link for this category in the main menu and in category listings. For example, to a landing page that contains a back link to the category.",
-                "Abweichender, externer Verweis für diese Warengruppe im Hauptmenü und in Warengruppen-Listings. Z.B. auf eine Landingpage, die einen Rückverweis auf die Warengruppe enthält.");
-
-            // Rule
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.TaxExempt", "Tax exempt", "Steuerbefreit");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.BillingCountry", "Billing country", "Rechnungsland");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.ShippingCountry", "Shipping country", "Lieferland");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LastActivityDays", "Last activity days", "Tage seit letztem Besuch");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CompletedOrderCount", "Completed order count", "Anzahl abgeschlossener Bestellungen");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CancelledOrderCount", "Cancelled order count", "Anzahl stornierter Bestellungen");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.NewOrderCount", "New order count", "Anzahl neuer Bestellungen");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.HasPurchasedProduct", "Has purchased product", "Hat eines der folgenden Produkt gekauft");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.HasPurchasedAllProducts", "Has purchased all products", "Hat alle folgenden Produkte gekauft");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.RuleSet", "Other rule set", "Anderer Regelsatz");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.Active", "Is active", "Ist aktiv");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LastLoginDays", "Last login days", "Tage seit letztem Login");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CreatedDays", "Created days", "Tage seit Registrierung");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.Salutation", "Salutation", "Anrede");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.Title", "Title", "Titel");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.Company", "Company", "Firma");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CustomerNumber", "Customer number", "Kundennummer");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.BirthDate", "Birthdate", "Geburtstag");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.Gender", "Gender", "Gender");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.ZipPostalCode", "Zip postal code", "Plz");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.VatNumberStatusId", "Vat number status", "USt-IdNr.");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.TimeZoneId", "Time zone", "Zeitzone");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.TaxDisplayTypeId", "Tax display type", "Steueranzeigetyp");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CountryId", "Country", "Land");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.CurrencyId", "Currency", "Währung");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LanguageId", "Language", "Sprache");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LastForumVisit", "Last forum visit days", "Tage seit letztem Forenbesuch");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LastUserAgent", "Last user agent", "Zuletzt genutzter User-Agent");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.IsInCustomerRole", "In customer role", "In Kundengruppe");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.StoreId", "Store", "Store");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.LastOrderDateDays", "Last order date days", "Tage seit letzter Bestellung");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.AcceptThirdPartyEmailHandOver", "Accept third party email handover", "Akzeptiert Weitergabe der Emailadresse an Dritte");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.OrderTotal", "Order total", "Gesamtbetrag der Bestellung");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.OrderSubtotalInclTax", "Order subtotal incl. tax", "Gesamtbetrag der Bestellung (Brutto)");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.OrderSubtotalExclTax", "Order subtotal excl tax", "Gesamtbetrag der Bestellung (Netto)");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.PaymentMethodSystemName", "Payment method systemname", "Systemname der Zahlart");
-            builder.AddOrUpdate("Admin.Rules.FilterDescriptor.ShippingRateComputationMethodSystemName", "Shipping rate computation method Systemname", "Systemname der Versandart");
-
-            builder.AddOrUpdate("Admin.Rules.SystemName", "System name", "Systemname");
-            builder.AddOrUpdate("Admin.Rules.Title", "Title", "Titel");
-            builder.AddOrUpdate("Admin.Rules.Execute", "{0} Execute {1} Rules", "Bedingungen {0} Ausführen {1}");
-            builder.AddOrUpdate("Admin.Configuration.RuleSets", "Rule sets", "Regelsätze");
-
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.Name", "Name", "Name");
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.Description", "Description", "Beschreibung");
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.IsActive", "Is active", "Ist aktiv");
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.Scope", "Scope", "Art");
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.IsSubGroup", "Is sub group", "Titel");
-            builder.AddOrUpdate("Admin.Rules.RuleSet.Fields.LogicalOperator", "Logical operator", "Logischer Operator");
-
-            // RuleOperators
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.Contains", "Contains", "Enthält");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.EndsWith", "Ends with", "Endet auf");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.GreaterThan", "Greater than", "Größer als");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.GreaterThanOrEqualTo", "Greater than or equal to", "Größer oder gleich");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.In", "In", "Ist eine von");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsEmpty", "Is empty", "Ist leer");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsEqualTo", "Is equal to", "Gleich");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsNotEmpty", "Is not empty", "Ist nicht leer");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsNotEqualTo", "Is not equal to", "Ungleich");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsNotNull", "Is not null", "Ist nicht NULL");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.IsNull", "Is null", "Ist NULL");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.LessThan", "Less than", "Kleiner als");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.LessThanOrEqualTo", "Less than or equal to", "Kleiner oder gleich");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.NotContains", "Not contains", "Enthält nicht");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.NotIn", "Not in", "Ist KEINE von");
-            builder.AddOrUpdate("Admin.Rules.RuleOperator.StartsWith", "Starts with", "Beginnt mit");
+            builder.Delete("Admin.Configuration.Measures.Weights.Fields.MarkAsPrimaryWeight");
         }
     }
 }
--- src/Presentation/SmartStore.Web/Administration/Controllers/MeasureController.cs
+++ src/Presentation/SmartStore.Web/Administration/Controllers/MeasureController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Web.Mvc;
 using SmartStore.Admin.Models.Directory;
 using SmartStore.Core.Domain.Directory;
-using SmartStore.Services.Configuration;
 using SmartStore.Services.Directory;
 using SmartStore.Services.Localization;
 using SmartStore.Services.Security;
@@ -14,66 +13,36 @@ using Telerik.Web.Mvc;
 
 namespace SmartStore.Admin.Controllers
 {
-	[AdminAuthorize]
+    [AdminAuthorize]
     public class MeasureController : AdminControllerBase
 	{
-		#region Fields
-
         private readonly IMeasureService _measureService;
+        private readonly ILanguageService _languageService;
+        private readonly ILocalizedEntityService _localizedEntityService;
         private readonly MeasureSettings _measureSettings;
-        private readonly ISettingService _settingService;
-        private readonly IPermissionService _permissionService;
-        private readonly ILocalizationService _localizationService;
 
-		#endregion
-
-		#region Constructors
-
-        public MeasureController(IMeasureService measureService,
-            MeasureSettings measureSettings, ISettingService settingService,
-            IPermissionService permissionService, ILocalizationService localizationService)
+        public MeasureController(
+            IMeasureService measureService,
+            ILanguageService languageService,
+            ILocalizedEntityService localizedEntityService,
+            MeasureSettings measureSettings)
 		{
-            this._measureService = measureService;
-            this._measureSettings = measureSettings;
-            this._settingService = settingService;
-            this._permissionService = permissionService;
-            this._localizationService = localizationService;
-		}
-
-		#endregion 
-
-		#region Methods
-        
+            _measureService = measureService;
+            _languageService = languageService;
+            _localizedEntityService = localizedEntityService;
+            _measureSettings = measureSettings;
+        }
+       
         #region Weights
 
-        public ActionResult Weights(string id)
+        public ActionResult Weights()
         {
-            if (!_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
-                return AccessDeniedView();
-
-            //mark as primary weight (if selected)
-            if (!String.IsNullOrEmpty(id))
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
             {
-                int primaryWeightId = Convert.ToInt32(id);
-                var primaryWeight = _measureService.GetMeasureWeightById(primaryWeightId);
-                if (primaryWeight != null)
-                {
-                    _measureSettings.BaseWeightId = primaryWeightId;
-                    _settingService.SaveSetting(_measureSettings);
-                }
+                return AccessDeniedView();
             }
 
-            var weightsModel = _measureService.GetAllMeasureWeights()
-                .Select(x => x.ToModel())
-                .ToList();
-            foreach (var wm in weightsModel)
-                wm.IsPrimaryWeight = wm.Id == _measureSettings.BaseWeightId;
-            var model = new GridModel<MeasureWeightModel>
-			{
-                Data = weightsModel,
-                Total = weightsModel.Count
-			};
-            return View(model);
+            return View();
 		}
 
 		[HttpPost, GridAction(EnableCustomBinding = true)]
@@ -81,7 +50,7 @@ namespace SmartStore.Admin.Controllers
         {
 			var model = new GridModel<MeasureWeightModel>();
 
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
 				var weightsModel = _measureService.GetAllMeasureWeights()
 					.Select(x => x.ToModel())
@@ -110,61 +79,149 @@ namespace SmartStore.Admin.Controllers
 		}
 
         [GridAction(EnableCustomBinding = true)]
-        public ActionResult WeightUpdate(MeasureWeightModel model, GridCommand command)
+        public ActionResult WeightDelete(int id, GridCommand command)
         {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
-				if (!ModelState.IsValid)
-				{
-					var modelStateErrors = this.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
-					return Content(modelStateErrors.FirstOrDefault());
-				}
+				var entity = _measureService.GetMeasureWeightById(id);
 
-				var weight = _measureService.GetMeasureWeightById(model.Id);
-				weight = model.ToEntity(weight);
-
-				_measureService.UpdateMeasureWeight(weight);
+                if (entity.Id == _measureSettings.BaseWeightId)
+                {
+                    NotifyError(T("Admin.Configuration.Measures.Weights.CantDeletePrimary"));
+                }
+                else
+                {
+                    _measureService.DeleteMeasureWeight(entity);
+                }
 			}
-            
-            return Weights(command);
-        }
-        
-        [GridAction(EnableCustomBinding = true)]
-        public ActionResult WeightAdd([Bind(Exclude="Id")] MeasureWeightModel model, GridCommand command)
-        {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
-			{
-				if (!ModelState.IsValid)
-				{
-					var modelStateErrors = this.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
-					return Content(modelStateErrors.FirstOrDefault());
-				}
 
-				var weight = new MeasureWeight();
-				weight = model.ToEntity(weight);
-
-				_measureService.InsertMeasureWeight(weight);
-			}
-            
             return Weights(command);
         }
 
-        [GridAction(EnableCustomBinding = true)]
-        public ActionResult WeightDelete(int id,  GridCommand command)
+        public ActionResult WeightCreatePopup()
         {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
-			{
-				var weight = _measureService.GetMeasureWeightById(id);
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
+            {
+                return AccessDeniedPartialView();
+            }
 
-				if (weight.Id == _measureSettings.BaseWeightId)
-				{
-					return Content(T("Admin.Configuration.Measures.Weights.CantDeletePrimary"));
-				}
+            var model = new MeasureWeightModel();
 
-				_measureService.DeleteMeasureWeight(weight);
-			}
+            AddLocales(_languageService, model.Locales);
 
-            return Weights(command);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult WeightCreatePopup(string btnId, MeasureWeightModel model)
+        {
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
+            {
+                return AccessDeniedPartialView();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var entity = new MeasureWeight();
+
+                try
+                {
+                    entity = model.ToEntity(entity);
+
+                    _measureService.InsertMeasureWeight(entity);
+
+                    if (model.IsPrimaryWeight)
+                    {
+                        _measureSettings.BaseWeightId = entity.Id;
+                        Services.Settings.SaveSetting(_measureSettings);
+                    }
+
+                    foreach (var localized in model.Locales)
+                    {
+                        _localizedEntityService.SaveLocalizedValue(entity, x => x.Name, localized.Name, localized.LanguageId);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View(model);
+                }
+
+                ViewBag.RefreshPage = true;
+                ViewBag.btnId = btnId;
+            }
+
+            return View(model);
+        }
+
+        public ActionResult WeightEditPopup(int id)
+        {
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
+            {
+                return AccessDeniedPartialView();
+            }
+
+            var entity = _measureService.GetMeasureWeightById(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = entity.ToModel();
+            model.IsPrimaryWeight = entity.Id == _measureSettings.BaseWeightId;
+
+            AddLocales(_languageService, model.Locales, (locale, languageId) =>
+            {
+                locale.Name = entity.GetLocalized(x => x.Name, languageId, false, false);
+            });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult WeightEditPopup(string btnId, MeasureWeightModel model)
+        {
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
+            {
+                return AccessDeniedPartialView();
+            }
+
+            var entity = _measureService.GetMeasureWeightById(model.Id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    entity = model.ToEntity(entity);
+
+                    _measureService.UpdateMeasureWeight(entity);
+
+                    if (model.IsPrimaryWeight && _measureSettings.BaseWeightId != entity.Id)
+                    {
+                        _measureSettings.BaseWeightId = entity.Id;
+                        Services.Settings.SaveSetting(_measureSettings);
+                    }
+
+                    foreach (var localized in model.Locales)
+                    {
+                        _localizedEntityService.SaveLocalizedValue(entity, x => x.Name, localized.Name, localized.LanguageId);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View(model);
+                }
+
+                ViewBag.RefreshPage = true;
+                ViewBag.btnId = btnId;
+            }
+
+            return View(model);
         }
 
         #endregion
@@ -173,31 +230,36 @@ namespace SmartStore.Admin.Controllers
 
         public ActionResult Dimensions(string id)
         {
-            if (!_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+            if (!Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
                 return AccessDeniedView();
 
-            //mark as primary dimension (if selected)
-            if (!String.IsNullOrEmpty(id))
+            // Mark as primary dimension (if selected).
+            if (id.HasValue())
             {
                 int primaryDimensionId = Convert.ToInt32(id);
                 var primaryDimension = _measureService.GetMeasureDimensionById(primaryDimensionId);
                 if (primaryDimension != null)
                 {
                     _measureSettings.BaseDimensionId = primaryDimensionId;
-                    _settingService.SaveSetting(_measureSettings);
+                    Services.Settings.SaveSetting(_measureSettings);
                 }
             }
 
             var dimensionsModel = _measureService.GetAllMeasureDimensions()
                 .Select(x => x.ToModel())
                 .ToList();
+
             foreach (var wm in dimensionsModel)
+            {
                 wm.IsPrimaryDimension = wm.Id == _measureSettings.BaseDimensionId;
+            }
+
             var model = new GridModel<MeasureDimensionModel>
             {
                 Data = dimensionsModel,
                 Total = dimensionsModel.Count
             };
+
             return View(model);
         }
 
@@ -206,7 +268,7 @@ namespace SmartStore.Admin.Controllers
         {
 			var model = new GridModel<MeasureDimensionModel>();
 
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
 				var dimensionsModel = _measureService.GetAllMeasureDimensions()
 					.Select(x => x.ToModel())
@@ -237,7 +299,7 @@ namespace SmartStore.Admin.Controllers
         [GridAction(EnableCustomBinding = true)]
         public ActionResult DimensionUpdate(MeasureDimensionModel model, GridCommand command)
         {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
 				if (!ModelState.IsValid)
 				{
@@ -257,7 +319,7 @@ namespace SmartStore.Admin.Controllers
         [GridAction(EnableCustomBinding = true)]
         public ActionResult DimensionAdd([Bind(Exclude="Id")] MeasureDimensionModel model, GridCommand command)
         {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
 				if (!ModelState.IsValid)
 				{
@@ -277,7 +339,7 @@ namespace SmartStore.Admin.Controllers
         [GridAction(EnableCustomBinding = true)]
         public ActionResult DimensionDelete(int id, GridCommand command)
         {
-			if (_permissionService.Authorize(StandardPermissionProvider.ManageMeasures))
+			if (Services.Permissions.Authorize(StandardPermissionProvider.ManageMeasures))
 			{
 				var dimension = _measureService.GetMeasureDimensionById(id);
 
@@ -291,8 +353,6 @@ namespace SmartStore.Admin.Controllers
 
             return Dimensions(command);
         }
-
-        #endregion
 
         #endregion
     }

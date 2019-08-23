5e63151308bf4b9b3a45c40cfb3f6cb55fb867dc
Marcus  Gesing <home@footwear-box.de>
2019-08-23 12:16:11 +0200

    Minor improvements on shipment details

--- src/Presentation/SmartStore.Web/Administration/Models/Orders/ShipmentModel.cs
+++ src/Presentation/SmartStore.Web/Administration/Models/Orders/ShipmentModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SmartStore.Core.Domain.Catalog;
 using SmartStore.Core.Domain.Common;
 using SmartStore.Web.Framework;
@@ -27,15 +28,19 @@ namespace SmartStore.Admin.Models.Orders
 
 		[SmartResourceDisplayName("Admin.Orders.Shipments.OrderID")]
         public int OrderId { get; set; }
+
         [SmartResourceDisplayName("Admin.Orders.Shipments.TotalWeight")]
         public string TotalWeight { get; set; }
+
         [SmartResourceDisplayName("Admin.Orders.Shipments.TrackingNumber")]
         public string TrackingNumber { get; set; }
+
         [SmartResourceDisplayName("Admin.Orders.Shipments.ShippedDate")]
-        public string ShippedDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
         public bool CanShip { get; set; }
+
         [SmartResourceDisplayName("Admin.Orders.Shipments.DeliveryDate")]
-        public string DeliveryDate { get; set; }
+        public DateTime? DeliveryDate { get; set; }
         public bool CanDeliver { get; set; }
 
         public List<ShipmentItemModel> Items { get; set; }

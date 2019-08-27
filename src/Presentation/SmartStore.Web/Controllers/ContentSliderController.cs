a638b7fbea035b7669f9cab729790b2a2a27b000
mkhafagy1986 <eng.mohammedkhafagy@gmail.com>
2019-08-28 01:43:21 +0300

    adding contentslider

---
+++ src/Presentation/SmartStore.Web/Controllers/ContentSliderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartStore.Web.Controllers
+{
+    public class ContentSliderController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult HomepageContentSlider()
+        {
+            return PartialView();
+        }
+    }
+}

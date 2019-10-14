a4531bc7cf02bdebd59445891802898e97426656
Marcus  Gesing <home@footwear-box.de>
2019-10-14 20:01:55 +0200

    Search: event added to take certain products into account for next indexing

---
+++ src/Libraries/SmartStore.Core/Search/Events/UpdateIndexEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SmartStore.Core.Search.Events
+{
+    /// <summary>
+    /// Publish event to index <see cref="EntityIds">entities with IDs</c> at next indexing.
+    /// </summary>
+    public class UpdateIndexEvent
+    {
+        public UpdateIndexEvent(string scope, IEnumerable<int> entityIds)
+        {
+            Guard.NotEmpty(scope, nameof(scope));
+            Guard.NotNull(entityIds, nameof(entityIds));
+
+            Scope = scope;
+            EntityIds = entityIds;
+        }
+
+        public string Scope
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<int> EntityIds
+        {
+            get;
+            private set;
+        }
+    }
+}

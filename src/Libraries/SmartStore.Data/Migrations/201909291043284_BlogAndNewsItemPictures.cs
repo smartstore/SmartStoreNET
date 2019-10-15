4a0cc72ef26bba7f6933051641b201c60a66b27b
Marcus  Gesing <home@footwear-box.de>
2019-10-15 20:50:09 +0200

    Fixes "The 'PreviewDisplayType' property on 'BlogPost' could not be set to a 'null' value"

--- src/Libraries/SmartStore.Data/Migrations/201909291043284_BlogAndNewsItemPictures.cs
+++ src/Libraries/SmartStore.Data/Migrations/201909291043284_BlogAndNewsItemPictures.cs
@@ -13,7 +13,7 @@ namespace SmartStore.Data.Migrations
             AddColumn("dbo.BlogPost", "SectionBg", c => c.String(maxLength: 100));
             AddColumn("dbo.BlogPost", "Intro", c => c.String());
             AddColumn("dbo.BlogPost", "DisplayTagsInPreview", c => c.Boolean(nullable: false));
-            AddColumn("dbo.BlogPost", "PreviewDisplayType", c => c.Int());
+            AddColumn("dbo.BlogPost", "PreviewDisplayType", c => c.Int(nullable: false));
             AddColumn("dbo.News", "PictureId", c => c.Int());
             AddColumn("dbo.News", "PreviewPictureId", c => c.Int());
         }

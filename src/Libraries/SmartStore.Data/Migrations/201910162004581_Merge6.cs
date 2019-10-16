18bc276d56a28f72404b710318eb3a6cb860be56
Marcus  Gesing <home@footwear-box.de>
2019-10-16 22:09:30 +0200

    Resolves #1723 Fixes installation failure. dbo.BlogPost and dbo.News domain model changes respectively BlogAndNewsItemPictures migration cause "Unable to update database to match the current model because there are pending changes..."

---
+++ src/Libraries/SmartStore.Data/Migrations/201910162004581_Merge6.cs
@@ -0,0 +1,16 @@
+namespace SmartStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge6 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

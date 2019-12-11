3e997457cabfdb3bd459376b8bf1ab0c41b32170
Marcus  Gesing <home@footwear-box.de>
2019-12-11 14:43:36 +0100

    DevTools: added permission provider TODO: add migration to avoid reinstallation of the plugin

---
+++ src/Plugins/SmartStore.DevTools/Models/ConfigurationModel.cs
@@ -0,0 +1,17 @@
+﻿using SmartStore.Web.Framework;
+using SmartStore.Web.Framework.Modelling;
+
+namespace SmartStore.DevTools.Models
+{
+    public class ConfigurationModel : ModelBase
+    {
+        [SmartResourceDisplayName("Plugins.Developer.DevTools.EnableMiniProfilerInPublicStore")]
+        public bool EnableMiniProfilerInPublicStore { get; set; }
+
+        [SmartResourceDisplayName("Plugins.Developer.DevTools.DisplayWidgetZones")]
+        public bool DisplayWidgetZones { get; set; }
+
+        [SmartResourceDisplayName("Plugins.Developer.DevTools.DisplayMachineName")]
+        public bool DisplayMachineName { get; set; }
+    }
+}
---
+++ src/Plugins/SmartStore.DevTools/Security/DevToolsPermissionProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SmartStore.Core.Domain.Customers;
+using SmartStore.Core.Domain.Security;
+using SmartStore.Core.Security;
+
+namespace SmartStore.DevTools.Security
+{
+    public static class DevToolsPermissions
+    {
+        public const string Self = "devtools";
+        public const string Read = "devtools.read";
+        public const string Update = "devtools.update";
+    }
+
+
+    public class DevToolsPermissionProvider : IPermissionProvider
+    {
+        public IEnumerable<PermissionRecord> GetPermissions()
+        {
+            var permissionSystemNames = PermissionHelper.GetPermissions(typeof(DevToolsPermissions));
+            var permissions = permissionSystemNames.Select(x => new PermissionRecord { SystemName = x });
+
+            return permissions;
+        }
+
+        public IEnumerable<DefaultPermissionRecord> GetDefaultPermissions()
+        {
+            return new[]
+            {
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Administrators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = DevToolsPermissions.Self }
+                    }
+                }
+            };
+        }
+    }
+}

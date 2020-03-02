1ea88b9d023eee0e97b1afc5ce5430d7afcba1c2
Marcel Schmidt <marcel.schmidt@smartstore.com>
2020-03-02 20:42:40 +0100

    Admin > Dashboard: *Initial commit, *Bestsellers report; *Initial best customers report;

---
+++ src/Presentation/SmartStore.Web/Administration/Models/Customers/BestCustomersDashboardReportModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SmartStore.Admin.Models.Customers;
+using SmartStore.Core.Domain.Customers;
+using SmartStore.Web.Framework.Modelling;
+
+namespace SmartStore.Admin.Models.Customers
+{
+    public class BestCustomersDashboardReportModel : ModelBase
+    {
+        public IList<BestCustomersReportLineModel> BestCustomersByQuantity { get; set; }
+        public IList<BestCustomersReportLineModel> BestCustomersByAmount { get; set; }
+    }
+}
---
+++ src/Presentation/SmartStore.Web/Administration/Models/Customers/BestCustomersReportLineModel.cs
@@ -0,0 +1,20 @@
+﻿using SmartStore.Web.Framework;
+using SmartStore.Web.Framework.Modelling;
+
+namespace SmartStore.Admin.Models.Customers
+{
+    public class BestCustomersReportLineModel : ModelBase
+    {
+		[SmartResourceDisplayName("Admin.Customers.Reports.BestBy.Fields.Customer")]
+        public int CustomerId { get; set; }
+
+        [SmartResourceDisplayName("Admin.Customers.Reports.BestBy.Fields.Customer")]
+        public string CustomerName { get; set; }
+
+        [SmartResourceDisplayName("Admin.Customers.Reports.BestBy.Fields.OrderTotal")]
+        public string OrderTotal { get; set; }
+
+        [SmartResourceDisplayName("Admin.Customers.Reports.BestBy.Fields.OrderCount")]
+        public decimal OrderCount { get; set; }
+    }
+}

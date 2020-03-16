44535d8f8a2de11aa5dfc4822f001b01d29e1acc
Marcel Schmidt <marcel.schmidt@smartstore.com>
2020-03-16 14:10:08 +0100

    Reworked dashboard model > for use as generic model for any chart

---
+++ src/Libraries/SmartStore.Core/Domain/Dashboard/DashboardChartReportLine.cs
@@ -0,0 +1,36 @@
+namespace SmartStore.Core.Domain.Dashboard
+{
+    public class DashboardChartReportLine
+    {
+        public DashboardChartReportLine(int amountDatasets, int amountDataPoints)
+        {
+            DataSets = new ChartDataPoint[amountDatasets];
+            Labels = new string[amountDataPoints];
+            for (int i = 0; i < DataSets.Length; i++)
+            {
+                DataSets[i] = new ChartDataPoint(amountDataPoints);
+            }
+        }
+
+        public string TotalAmount { get; set; }
+        public int PercentageDelta { get; set; }
+        public string[] Labels { get; set; }
+
+        public ChartDataPoint[] DataSets { get; set; }       
+    }
+
+    public class ChartDataPoint
+    {
+        public ChartDataPoint(int amountDataPoints)
+        {
+            Quantity = new int[amountDataPoints];
+            Amount = new decimal[amountDataPoints];
+            FormattedAmount = new string[amountDataPoints];
+        }
+
+        public string TotalAmount { get; set; }
+        public int[] Quantity { get; set; }
+        public decimal[] Amount { get; set; }
+        public string[] FormattedAmount { get; set; }
+    }
+}

356e4b89033ffe4945a3e484bcc1790605e0a4e1
Marcel Schmidt <marcel.schmidt@smartstore.com>
2020-03-19 21:39:29 +0100

    Orders incomplete > minor changes, labelling

--- src/Presentation/SmartStore.Web/Administration/Models/Orders/OrdersIncompleteDashboardReportModel.cs
+++ src/Presentation/SmartStore.Web/Administration/Models/Orders/OrdersIncompleteDashboardReportModel.cs
@@ -14,11 +14,13 @@ namespace SmartStore.Admin.Models.Orders
         //[SmartResourceDisplayName("Admin.SalesReport.Incomplete.Item")]
         //public string Item { get; set; }
 
-        [SmartResourceDisplayName("Admin.SalesReport.Incomplete.Total")]
+        //[SmartResourceDisplayName("Admin.SalesReport.Incomplete.Total")]
         public string AmountTotal { get; set; }
 
-        [SmartResourceDisplayName("Admin.SalesReport.Incomplete.Count")]
+        //[SmartResourceDisplayName("Admin.SalesReport.Incomplete.Count")]
         public int Quantity { get; set; }
+
+        public string FormattedQuantity { get; set; }
 
         //public string Url { get; set; }
     }

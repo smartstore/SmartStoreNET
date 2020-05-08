662419d563b630519df41e29c78985999b4f239f
Marcus  Gesing <gesing@smartstore.com>
2020-05-08 14:10:23 +0200

    Rule builder: there is no provider for scope OrderItem at the moment

--- src/Libraries/SmartStore.Core/Rules/RuleScope.cs
+++ src/Libraries/SmartStore.Core/Rules/RuleScope.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartStore.Rules
+﻿namespace SmartStore.Rules
 {
     public enum RuleScope
     {
         Cart = 0,
-        OrderItem = 1,
+        //OrderItem = 1,
         Customer = 2,
         Product = 3,
     }

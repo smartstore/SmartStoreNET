23896c214049236da611a64f59d96c81db01005a
Murat Cakir <muratc@smartstore.de>
2020-08-26 23:14:29 +0200

    Minor cleanup

--- src/Libraries/SmartStore.Services/Events/ConsumerResolver.cs
+++ src/Libraries/SmartStore.Services/Events/ConsumerResolver.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using SmartStore.Core;
 using SmartStore.Core.Events;

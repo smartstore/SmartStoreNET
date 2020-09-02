84b0c98bbd6f05e7e0dbab75d2d8f730f1a12ffe
Marcus  Gesing <gesing@smartstore.com>
2020-09-02 19:10:58 +0200

    Installation: fixes "Unable to update database to match the current model because there are pending changes"

---
+++ src/Libraries/SmartStore.Data/Migrations/202009021705132_Merge9.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge9 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

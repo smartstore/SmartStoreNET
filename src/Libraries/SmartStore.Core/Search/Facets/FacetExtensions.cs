9ce738f8e8dad21d32fd8689da0174a51bdc9103
Marcus  Gesing <gesing@smartstore.com>
2020-09-07 17:02:39 +0200

    Minor improvement at sorting of facets

--- src/Libraries/SmartStore.Core/Search/Facets/FacetExtensions.cs
+++ src/Libraries/SmartStore.Core/Search/Facets/FacetExtensions.cs
@@ -33,27 +33,38 @@ namespace SmartStore.Core.Search.Facets
 			{
 				case FacetSorting.ValueAsc:
 					if (selectedFirst)
-						return source.OrderByDescending(x => x.Value.IsSelected).ThenBy(x => x.Value.Value);
+						return source
+							.OrderByDescending(x => x.Value.IsSelected)
+							.ThenBy(x => x.Value.Value);
 					else
 						return source.OrderBy(x => x.Value.Value);
 
 				case FacetSorting.LabelAsc:
 					if (selectedFirst)
-						return source.OrderByDescending(x => x.Value.IsSelected).ThenBy(x => x.Value.Label);
+						return source
+							.OrderByDescending(x => x.Value.IsSelected)
+							.ThenBy(x => x.Value.Label);
 					else
 						return source.OrderBy(x => x.Value.Label);
 
 				case FacetSorting.DisplayOrder:
 					if (selectedFirst)
-						return source.OrderByDescending(x => x.Value.IsSelected).ThenBy(x => x.Value.DisplayOrder);
+						return source
+							.OrderByDescending(x => x.Value.IsSelected)
+							.ThenBy(x => x.Value.DisplayOrder);
 					else
 						return source.OrderBy(x => x.Value.DisplayOrder);
 
 				default:
 					if (selectedFirst)
-						return source.OrderByDescending(x => x.Value.IsSelected).ThenByDescending(x => x.HitCount);
+						return source
+							.OrderByDescending(x => x.Value.IsSelected)
+							.ThenByDescending(x => x.HitCount)
+							.ThenBy(x => x.Value.Label);
 					else
-						return source.OrderByDescending(x => x.HitCount);
+						return source
+							.OrderByDescending(x => x.HitCount)
+							.ThenBy(x => x.Value.Label);
 			}
 		}
 

1610756a73776018915e6b025df3d120d483c160
Michael Herzog <herzog@smartstore.com>
2020-09-08 20:28:27 +0200

    Allow HTML for SEO meta infos

--- src/Presentation/SmartStore.Web.Framework/Seo/ISeoModel.cs
+++ src/Presentation/SmartStore.Web.Framework/Seo/ISeoModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web.Mvc;
 using SmartStore.Web.Framework.Localization;
 
 namespace SmartStore.Web.Framework.Seo
@@ -6,12 +7,15 @@ namespace SmartStore.Web.Framework.Seo
     public interface ISeoModel : ILocalizedModel<SeoModelLocal>
     {
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaTitle")]
+        [AllowHtml]
         string MetaTitle { get; }
 
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaDescription")]
+        [AllowHtml]
         string MetaDescription { get; }
 
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaKeywords")]
+        [AllowHtml]
         string MetaKeywords { get; }
     }
 
@@ -20,12 +24,15 @@ namespace SmartStore.Web.Framework.Seo
         public int LanguageId { get; set; }
 
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaTitle")]
+        [AllowHtml]
         public string MetaTitle { get; set; }
 
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaDescription")]
+        [AllowHtml]
         public string MetaDescription { get; set; }
 
         [SmartResourceDisplayName("Admin.Configuration.Seo.MetaKeywords")]
+        [AllowHtml]
         public string MetaKeywords { get; set; }
     }
 }

1625b09cd7f21961edc179619e4a3be49d595ce5
Murat Cakir <muratc@smartstore.de>
2020-09-18 20:26:11 +0200

    Textarea should be disabled when multi-store-override-checkbox is off

--- src/Presentation/SmartStore.Web/Administration/Scripts/admin.common.js
+++ src/Presentation/SmartStore.Web/Administration/Scripts/admin.common.js
@@ -31,7 +31,7 @@ SmartStore.Admin = {
             parent = parentSelector ? $(parentSelector) : checkbox.closest('.multi-store-setting-group').find('> .multi-store-setting-control'),
             checked = checkbox.is(':checked');
 
-        parent.find('input:not([type=hidden]), select').each(function (i, el) {
+        parent.find('input:not([type=hidden]), select, textarea').each(function (i, el) {
             var input = $(el);
             var tbox = input.data('tTextBox');
 

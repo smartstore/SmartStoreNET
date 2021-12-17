626ce90f4d3d5f0828be8eb781eec3d39273cd75
Michael Herzog <herzog@smartstore.com>
2021-12-17 13:54:40 +0100

    Updated resources for 4.2.0

--- src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
+++ src/Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
@@ -40,12 +40,6 @@
         {
             context.MigrateLocaleResources(MigrateLocaleResources);
             MigrateSettings(context);
-
-            if (DataSettings.DatabaseIsInstalled())
-            {
-                var logTypeMigrator = new ActivityLogTypeMigrator(context);
-                logTypeMigrator.AddActivityLogType("EditOrder", "Edit an order", "Auftrag bearbeitet");
-            }
         }
 
         public void MigrateSettings(SmartObjectContext context)
@@ -55,15 +49,6 @@
 
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
-            builder.AddOrUpdate("Admin.Configuration.Settings.GeneralCommon.SocialSettings.TwitterSite.Error",
-                "The Twitter username must begin with an '@'.",
-                "Der Twitter-Benutzername muss mit einem '@' beginnen.");
-
-            builder.AddOrUpdate("Admin.Configuration.Settings.ShoppingCart.AddProductsToBasketInSinglePositions",
-                "Add products to cart in single positions",
-                "Produkte in einzelnen Positionen in den Warenkorb legen",
-                "Enable this option if you want products with different quantities to be added to the shopping cart in single position.",
-                "Aktivieren Sie diese Option, wenn Produkte mit verschiedenen Mengenangaben als Einzelpositionen in den Warenkorb gelegt werden sollen.");
         }
     }
 }
